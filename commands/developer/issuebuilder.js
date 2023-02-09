08e5f39b09e7f4b4d5e7cbabbeac1e794fd1f8af
Yoru <m.f.fetvaci@hotmail.com>
2023-02-09 20:15:08 +0300

    issue builder v2

--- commands/developer/issuebuilder.js
+++ commands/developer/issuebuilder.js
@@ -1,7 +1,7 @@
 const { Octokit } = require('octokit');
-const { EmbedBuilder } = require('discord.js')
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js')
 
-exports.run = async (client, message, args, prefix, EmbedBuilder) => {
+exports.run = async (client, message, args, prefix) => {
     await message.channel.sendTyping()
 
     if (message.author.id != "372343076578131968") {
@@ -13,55 +13,210 @@ exports.run = async (client, message, args, prefix, EmbedBuilder) => {
         auth: process.env.Github_Token
     })
 
-    let string = args.join(" ").match(/"(.*?)"/)
-    if (string) {
-        Title = string[1]
-    } else {
-        Title = args[0]
-    }
+    let Title = args.join(" ")
 
 
 
     let labels = []
 
-    if (args.includes("-bug")) labels.push("bug")
-    if (args.includes("-documentation")) labels.push("documentation")
-    if (args.includes("-duplicate")) labels.push("duplicate")
-    if (args.includes("-priority")) labels.push("high priority")
-    if (args.includes("-enhancement")) labels.push("enhancement")
-    if (args.includes("-firstissue")) labels.push("good first issue")
-    if (args.includes("-new")) labels.push("new command")
-    if (args.includes("-suggestion")) labels.push("suggestion")
-    if (args.includes("-wontfix")) labels.push("wontfix")
+
+
+
+    user = message.author
+
+    try {
+        if (message.reference.messageId) {
+
+            message.channel.messages.fetch(message.reference.messageId).then(async Content => {
+
+                let addedlabels = "None"
+                const confirm = new EmbedBuilder()
+                    .setTitle("Issue builder v2")
+                    .setThumbnail(`${user.displayAvatarURL()}?size=1024`)
+                    .setDescription(`Preview of issue:\n\n**${Title}**\n${Content.content}`)
+                    .setFields({ name: `Added labels:`, value: addedlabels })
+
+
+                const row = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setLabel("Bug")
+                            .setCustomId("bug")
+                            .setStyle(ButtonStyle.Secondary),
+
+                        new ButtonBuilder()
+                            .setLabel("Suggestion")
+                            .setCustomId("suggestion")
+                            .setStyle(ButtonStyle.Secondary),
+
+                        new ButtonBuilder()
+                            .setLabel("High priority")
+                            .setCustomId("priority")
+                            .setStyle(ButtonStyle.Secondary),
+
+                    )
+
+                const row2 = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setLabel("New command")
+                            .setCustomId("new")
+                            .setStyle(ButtonStyle.Secondary),
+
+                        new ButtonBuilder()
+                            .setLabel("First issue")
+                            .setCustomId("first")
+                            .setStyle(ButtonStyle.Secondary),
+
+                    )
+
+                const row3 = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setLabel("Confirm")
+                            .setCustomId("confirm")
+                            .setStyle(ButtonStyle.Success),
+
+                        new ButtonBuilder()
+                            .setLabel("Cancel")
+                            .setCustomId("cancel")
+                            .setStyle(ButtonStyle.Danger),
+
+                    )
+
+                message.channel.send({ embeds: [confirm], components: [row, row2, row3] })
+
+
+
+                const collector = message.channel.createMessageComponentCollector()
+
+                let addLabels = []
+
+                try {
+                    collector.on("collect", async (i) => {
+                        try {
+
+                            if (i.user.id != "372343076578131968") return;
+
+
+                            if (i.customId == "bug") {
+                                console.log(addLabels)
+                                addLabels.push("bug")
+                                console.log(addLabels)
+
+                                const confirm = new EmbedBuilder()
+                                    .setTitle("Issue builder v2")
+                                    .setThumbnail(`${user.displayAvatarURL()}?size=1024`)
+                                    .setDescription(`Preview of issue:\n\n**${Title}**\n${Content.content}`)
+                                    .setFields({ name: `Added labels:`, value: addLabels.join(", ") })
+
+                                i.update({ embeds: [confirm], components: [row, row2, row3] })
+                            }
+
+                            if (i.customId == "suggestion") {
+                                console.log(addLabels)
+                                addLabels.push("suggestion")
+                                console.log(addLabels)
+
+                                const confirm = new EmbedBuilder()
+                                    .setTitle("Issue builder v2")
+                                    .setThumbnail(`${user.displayAvatarURL()}?size=1024`)
+                                    .setDescription(`Preview of issue:\n\n**${Title}**\n${Content.content}`)
+                                    .setFields({ name: `Added labels:`, value: addLabels.join(", ") })
+
+                                i.update({ embeds: [confirm], components: [row, row2, row3] })
+                            }
+
+                            if (i.customId == "priority") {
+                                console.log(addLabels)
+                                addLabels.push("high priority")
+                                console.log(addLabels)
+
+                                const confirm = new EmbedBuilder()
+                                    .setTitle("Issue builder v2")
+                                    .setThumbnail(`${user.displayAvatarURL()}?size=1024`)
+                                    .setDescription(`Preview of issue:\n\n**${Title}**\n${Content.content}`)
+                                    .setFields({ name: `Added labels:`, value: addLabels.join(", ") })
+
+                                i.update({ embeds: [confirm], components: [row, row2, row3] })
+                            }
+
+                            if (i.customId == "new") {
+                                console.log(addLabels)
+                                addLabels.push("new command")
+                                console.log(addLabels)
+
+                                const confirm = new EmbedBuilder()
+                                    .setTitle("Issue builder v2")
+                                    .setThumbnail(`${user.displayAvatarURL()}?size=1024`)
+                                    .setDescription(`Preview of issue:\n\n**${Title}**\n${Content.content}`)
+                                    .setFields({ name: `Added labels:`, value: addLabels.join(", ") })
+
+                                i.update({ embeds: [confirm], components: [row, row2, row3] })
+                            }
+
+                            if (i.customId == "first") {
+                                console.log(addLabels)
+                                addLabels.push("good first issue")
+                                console.log(addLabels)
+
+                                const confirm = new EmbedBuilder()
+                                    .setTitle("Issue builder v2")
+                                    .setThumbnail(`${user.displayAvatarURL()}?size=1024`)
+                                    .setDescription(`Preview of issue:\n\n**${Title}**\n${Content.content}`)
+                                    .setFields({ name: `Added labels:`, value: addLabels.join(", ") })
+
+                                i.update({ embeds: [confirm], components: [row, row2, row3] })
+                            }
+
+
+
+                            if (i.customId == 'cancel') {
+                                console.log("hii")
+                                i.update({ content: "Successfully canceled issue builder.", embeds: [], components: [] })
+                                return;
+                            }
+
+                            if (i.customId == "confirm") {
+
+                                const issuebuilder = await octokit.request('POST /repos/YoruNoKen/miaosu/issues', {
+                                    owner: 'YoruNoKen',
+                                    repo: 'miaosu',
+                                    title: `${Title}`,
+                                    body: `> ${Content.content}\n\n[Original Message by @${Content.author.tag}](https://canary.discord.com/channels/${Content.guildId}/${Content.channelId}/${Content.id})`,
+                                    labels: addLabels
+                                })
+
+                                const labelNames = issuebuilder.data.labels.map(x => x.name).join(", ")
+                                const embed = new EmbedBuilder()
+                                    .setTitle("Issue builder v2")
+                                    .setColor("Purple")
+                                    .setDescription(`Successfully created Issue #${issuebuilder.data.number}\n[Click here to go to issue](https://github.com/YoruNoKen/miaosu/issues/${issuebuilder.data.number})`)
+                                    .setFields(
+                                        { name: `Added Labels:`, value: labelNames },
+                                        { name: `Assigned:`, value: `YoruNoKen` }
+                                    )
+
+                                i.update({ embeds: [embed], components: [] })
+
+                            }
+                        } catch (err) {
+                            console.log(err)
+                        }
+
+                    })
+                } catch (err) { }
 
 
 
 
-    
-    
-    if (message.reference.messageId) {
-        message.channel.messages.fetch(message.reference.messageId).then(async Content => {
-            
-            const issuebuilder = await octokit.request('POST /repos/YoruNoKen/miaosu/issues', {
-                owner: 'YoruNoKen',
-                repo: 'miaosu',
-                title: `${Title}`,
-                body: `> ${Content.content}\n\n[Original Message by @${Content.author.tag}](https://canary.discord.com/channels/${Content.guildId}/${Content.channelId}/${Content.id})`,
-                labels: labels
             })
-            
-            const labelNames = issuebuilder.data.labels.map(x => x.name).join(", ")
-            const embed = new EmbedBuilder()
-                .setTitle("Successful!")
-                .setColor("Purple")
-                .setDescription(`Successfully created Issue #${issuebuilder.data.number}\n[Click here to go to issue](https://github.com/YoruNoKen/miaosu/issues/${issuebuilder.data.number})`)
-                .setFields(
-                    { name: `Added Labels:`, value: labelNames },
-                    { name: `Assigned:`, value: `YoruNoKen` }
-                )
 
-            message.channel.send({ embeds: [embed] })
-        })
+        }
+
+    } catch (err) {
+        message.reply("**Please reply to a message.**")
+        return;
     }
 
 
