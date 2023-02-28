083ed7be0c43ddc5c942bd60de91e4e3546162ad
Yoru <m.f.fetvaci@hotmail.com>
2023-02-28 18:58:48 +0300

    updated leaderboard command to include detaild accuracy

--- src/commands/osu/leaderboard.js
+++ src/commands/osu/leaderboard.js
@@ -1,218 +1,211 @@
 // require('fetch')
-const axios = require('axios');
-const { v2, auth, mods, tools } = require("osu-api-extended");
+const axios = require("axios")
+const { v2, auth, mods, tools } = require("osu-api-extended")
 
 // importing GetRecent
-const { LbSend } = require('../../exports/leaderboard_export.js')
+const { LbSend } = require("../../exports/leaderboard_export.js")
 exports.run = async (client, message, args, prefix) => {
-  await message.channel.sendTyping()
+	await message.channel.sendTyping()
 
-  await auth.login(process.env.client_id, process.env.client_secret);
-  let EmbedValue = 0
-  let GoodToGo = false
+	await auth.login(process.env.client_id, process.env.client_secret)
+	let EmbedValue = 0
+	let GoodToGo = false
+	let ModeSelected = false
+	let GameMode
 
-  let pagenum = 1
-  if (args.includes('-p')) {
-    const iIndex = args.indexOf('-p')
-    pagenum = Number(args[iIndex + 1])
-    value = undefined
-  } else {
-    pagenum = 1
-  }
+	let pagenum = 1
+	if (args.includes("-p")) {
+		const iIndex = args.indexOf("-p")
+		pagenum = Number(args[iIndex + 1])
+		value = undefined
+	}
 
-  let GameMode = "osu"
-  let RuleSetId = 0
+	async function GetMapMode(beatmapId) {
+		const mapinfo = await v2.beatmap.diff(beatmapId)
+		if (GameMode == undefined) GameMode = mapinfo.mode
+		return { GameMode, mapinfo }
+	}
 
-  if (args.includes('-taiko')) {
-    RuleSetId = 1
-    GameMode = "taiko"
-  }
-  if (args.includes('-mania')) {
-    RuleSetId = 3
-    GameMode = "mania"
-  }
-  if (args.includes('-ctb')) {
-    RuleSetId = 2
-    GameMode = "fruits"
-  }
+	if (args.includes("-taiko")) {
+		GameMode = "taiko"
+		ModeSelected = true
+	}
+	if (args.includes("-mania")) {
+		GameMode = "mania"
+		ModeSelected = true
+	}
+	if (args.includes("-ctb")) {
+		GameMode = "fruits"
+		ModeSelected = true
+	}
 
-  // determine the page of the lb
-  const start = (pagenum - 1) * 5 + 1;
-  const end = pagenum * 5;
-  const numbers = [];
-  for (let i = start; i <= end; i++) {
-    numbers.push(i);
-  }
-  one = numbers[0] - 1;
-  two = numbers[1] - 1;
-  three = numbers[2] - 1;
-  four = numbers[3] - 1;
-  five = numbers[4] - 1;
-  //grades
-  const grades = {
-    A: "<:A_:1057763284327080036>",
-    B: "<:B_:1057763286097076405>",
-    C: "<:C_:1057763287565086790>",
-    D: "<:D_:1057763289121173554>",
-    F: "<:F_:1057763290484318360>",
-    S: "<:S_:1057763291998474283>",
-    SH: "<:SH_:1057763293491642568>",
-    X: "<:X_:1057763294707974215>",
-    XH: "<:XH_:1057763296717045891>",
-  };
+	// determine the page of the lb
+	const start = (pagenum - 1) * 5 + 1
+	const end = pagenum * 5
+	const numbers = []
+	for (let i = start; i <= end; i++) {
+		numbers.push(i)
+	}
+	one = numbers[0] - 1
+	two = numbers[1] - 1
+	three = numbers[2] - 1
+	four = numbers[3] - 1
+	five = numbers[4] - 1
 
+	let modsArg
+	let SortArg = ""
+	let modifiedMods
+	if (args.join(" ").includes("+")) {
+		const iIndex = args.indexOf("+")
+		modsArg = args[iIndex + 1]
+			.slice(1)
+			.toUpperCase()
+			.match(/[A-Z]{2}/g)
+		SortArg = `, Sorting by: ${modsArg}`
+		if (args[0].startsWith("https://")) {
+			modsArg = args[iIndex + 2]
+				.slice(1)
+				.toUpperCase()
+				.match(/[A-Z]{2}/g)
+		}
+		modifiedMods = modsArg.map(mod => `&mods[]=${mod}`).join("")
+	} else {
+		modifiedMods = ""
+		SortArg = ""
+	}
 
-  let modsArg;
-  let SortArg = "";
-  let modifiedMods
-  if (args.join(" ").includes("+")) {
-    const iIndex = args.indexOf("+")
-    modsArg = (args[iIndex + 1].slice(1)).toUpperCase().match(/[A-Z]{2}/g)
-    SortArg = `, Sorting by: ${modsArg}`
-    if (args[0].startsWith("https://")) {
-      modsArg = (args[iIndex + 2].slice(1)).toUpperCase().match(/[A-Z]{2}/g)
-    }
-    modifiedMods = modsArg.map((mod) => `&mods[]=${mod}`).join("")
-  } else {
-    modifiedMods = ""
-    SortArg = "";
-  }
+	async function SendEmbed(beatmapId, scores, pagenum, mapinfo) {
+		let ModeText
+		if (mapinfo.mode_int == "0") ModeText = ", Standard"
+		if (mapinfo.mode_int == "1") ModeText = ", Taiko"
+		if (mapinfo.mode_int == "2") ModeText = ", Ctb"
+		if (mapinfo.mode_int == "3") ModeText = ", Mania"
 
+		message.channel.send({ content: `Global LB${SortArg}${ModeText}`, embeds: [await LbSend(beatmapId, scores, pagenum, mapinfo)] })
+	}
 
-  async function SendEmbed(beatmapId, scores, pagenum) {
-    message.channel.send({content: `Global LB${SortArg}`,embeds: [await LbSend(beatmapId, scores, pagenum)]})
-  }
+	async function EmbedFetch(embed) {
+		try {
+			const embed_author = embed.url
+			const beatmapId = embed_author.match(/\d+/)[0]
+			if (!ModeSelected) GameMode = await (await GetMapMode(beatmapId)).GameMode
+			const mapinfo = await (await GetMapMode(beatmapId)).mapinfo
+			if (mapinfo.id == undefined) throw new Error("Wrong embed")
 
-  async function EmbedFetch(embed) {
-    try {
-      const embed_author = embed.url
-      const beatmapId = embed_author.match(/\d+/)[0]
+			const response = await axios.get(`https://osu.ppy.sh/beatmaps/${beatmapId}/scores?mode=${GameMode}&type=global${modifiedMods}`, { headers: { Cookie: `osu_session=${process.env.OSU_SESSION}` } })
+			const scores = response.data
+			//send the embed
+			SendEmbed(beatmapId, scores, pagenum, mapinfo)
+			GoodToGo = true
+		} catch (err) {
+			console.log(err)
 
-      console.log(`url ${beatmapId}`)
-      const response = await axios.get(`https://osu.ppy.sh/beatmaps/${beatmapId}/scores?mode=${GameMode}&type=global${modifiedMods}`, { headers: { Cookie: `osu_session=${process.env.OSU_SESSION}` } })
-      const scores = response.data
-      //send the embed
-      SendEmbed(beatmapId, scores, pagenum)
-      GoodToGo = true
+			console.log("err found, switching to author")
 
+			try {
+				const embed_author = embed.author.url
+				const beatmapId = embed_author.match(/\d+/)[0]
+				if (!ModeSelected) GameMode = await (await GetMapMode(beatmapId)).GameMode
+				const mapinfo = await (await GetMapMode(beatmapId)).mapinfo
+				if (mapinfo.id == undefined) throw new Error("Wrong embed")
 
-    } catch (err) {
-      console.log(err)
+				const response = await axios.get(`https://osu.ppy.sh/beatmaps/${beatmapId}/scores?mode=${GameMode}&type=global${modifiedMods}`, { headers: { Cookie: `osu_session=${process.env.OSU_SESSION}` } })
+				const scores = response.data
+				//send the embed
+				SendEmbed(beatmapId, scores, pagenum, mapinfo)
+				GoodToGo = true
+			} catch (err) {
+				console.log(err)
 
-      console.log('err found, switching to author')
+				console.log("err found, switching to desc")
+				try {
+					const regex = /\/b\/(\d+)/
+					const match = regex.exec(embed.description)
+					const beatmapId = match[1]
+					if (!ModeSelected) GameMode = await (await GetMapMode(beatmapId)).GameMode
+					const mapinfo = await (await GetMapMode(beatmapId)).mapinfo
+					if (mapinfo.id == undefined) throw new Error("Wrong embed")
 
+					const response = await axios.get(`https://osu.ppy.sh/beatmaps/${beatmapId}/scores?mode=${GameMode}&type=global${modifiedMods}`, { headers: { Cookie: `osu_session=${process.env.OSU_SESSION}` } })
+					const scores = response.data
 
-      try {
-        const embed_author = embed.author.url
-        const beatmapId = embed_author.match(/\d+/)[0]
-        console.log(`url ${beatmapId}`)
-        const response = await axios.get(`https://osu.ppy.sh/beatmaps/${beatmapId}/scores?mode=${GameMode}&type=global${modifiedMods}`, { headers: { Cookie: `osu_session=${process.env.OSU_SESSION}` } })
-        const scores = response.data
-        //send the embed
-        SendEmbed(beatmapId, scores, pagenum)
-        GoodToGo = true
+					//send the embed
+					SendEmbed(beatmapId, scores, pagenum, mapinfo)
+					GoodToGo = true
+					return
+				} catch (err) {
+					console.log(err)
+					EmbedValue++
+				}
+			}
+		}
+	}
 
-      } catch (err) {
-        console.log(err)
+	if (message.mentions.users.size > 0 && message.mentions.repliedUser.bot) {
+		message.channel.messages.fetch(message.reference.messageId).then(message => {
+			const embed = message.embeds[0]
 
-        console.log('err found, switching to desc')
-        try {
-          const regex = /\/b\/(\d+)/;
-          const match = regex.exec(embed.description);
-          const beatmapId = match[1];
+			EmbedFetch(embed)
+		})
+		return
+	}
 
-          console.log(`url ${beatmapId}`)
-          const response = await axios.get(`https://osu.ppy.sh/beatmaps/${beatmapId}/scores?mode=${GameMode}&type=global${modifiedMods}`, { headers: { Cookie: `osu_session=${process.env.OSU_SESSION}` } })
-          const scores = response.data
-          //send the embed
-          SendEmbed(beatmapId, scores, pagenum)
-          GoodToGo = true
-          return;
-        } catch (err) {
-          console.log(err)
-          EmbedValue++
-        }
-      }
-    }
-  }
+	const channel = client.channels.cache.get(message.channel.id)
+	channel.messages.fetch({ limit: 100 }).then(async messages => {
+		//find the latest message with an embed
+		let embedMessages = []
+		for (const [id, message] of messages) {
+			if (message.embeds.length > 0 && message.author.bot) {
+				embedMessages.push(message)
+			}
+		}
 
+		try {
+			if (args) {
+				//try to get beatmapId by link
+				const regex = /\/(\d+)$/
+				const match = regex.exec(args[0])
+				const beatmapId = match[1]
+				//if args doesn't start with https: try to get the beatmap id by number provided
+				if (!args[0].startsWith("https:")) {
+					beatmapId = args[0]
+				}
 
-  if (message.mentions.users.size > 0 && message.mentions.repliedUser.bot) {
-    message.channel.messages.fetch(message.reference.messageId).then(message => {
-      const embed = message.embeds[0]
+				if (!ModeSelected) GameMode = await (await GetMapMode(beatmapId)).GameMode
+				const mapinfo = await (await GetMapMode(beatmapId)).mapinfo
+				if (mapinfo.id == undefined) throw new Error("Wrong embed")
 
-      EmbedFetch(embed)
-    })
-    return;
-
-  }
-
-
-
-  const channel = client.channels.cache.get(message.channel.id);
-  channel.messages.fetch({ limit: 100 }).then(async messages => {
-
-    //find the latest message with an embed
-    let embedMessages = [];
-    for (const [id, message] of messages) {
-      if (message.embeds.length > 0 && message.author.bot) {
-        embedMessages.push(message);
-      }
-    }
-
-    try {
-      if (args) {
-        //try to get beatmapId by link
-        const regex = /\/(\d+)$/
-        const match = regex.exec(args[0])
-        const beatmapId = match[1]
-        //if args doesn't start with https: try to get the beatmap id by number provided
-        if (!args[0].startsWith("https:")) {
-          beatmapId = args[0]
-        }
-        const map = await v2.beatmap.diff(beatmapId)
-        map.id
-
-        //message
-        try {
-          //send the embed
-          const response = await axios.get(`https://osu.ppy.sh/beatmaps/${beatmapId}/scores?mode=${GameMode}&type=global${modifiedMods}`, { headers: { Cookie: `osu_session=${process.env.OSU_SESSION}` } })
-          const scores = response.data
-          SendEmbed(beatmapId, scores, pagenum)
-          return;
-        } catch (err) {
-          // console.log(err)
-        }
-      }
-    } catch (err) {
-
-      try {
-        if (embedMessages) {
-          do {
-            if (!embedMessages[EmbedValue].embeds[0]) break;
-            const embed = embedMessages[EmbedValue].embeds[0];
-            await EmbedFetch(embed)
-            console.log(GoodToGo)
-          }
-          while (!GoodToGo)
-
-        } else {
-          await message.channel.send('No embeds found in the last 100 messages');
-        }
-      } catch (err) {
-        message.channel.send("**No maps found**")
-      }
-
-    }
-
-
-
-  });
-
-
-};
-exports.name = "leadearboard";
+				//message
+				try {
+					//send the embed
+					const response = await axios.get(`https://osu.ppy.sh/beatmaps/${beatmapId}/scores?mode=${GameMode}&type=global${modifiedMods}`, { headers: { Cookie: `osu_session=${process.env.OSU_SESSION}` } })
+					const scores = response.data
+					SendEmbed(beatmapId, scores, pagenum, mapinfo)
+					return
+				} catch (err) {
+					// console.log(err)
+				}
+			}
+		} catch (err) {
+			try {
+				if (embedMessages) {
+					do {
+						if (!embedMessages[EmbedValue].embeds[0]) break
+						const embed = embedMessages[EmbedValue].embeds[0]
+						await EmbedFetch(embed)
+						console.log(GoodToGo)
+					} while (!GoodToGo)
+				} else {
+					await message.channel.send("No embeds found in the last 100 messages")
+				}
+			} catch (err) {
+				message.channel.send("**No maps found**")
+			}
+		}
+	})
+}
+exports.name = "leadearboard"
 exports.aliases = ["lb", "leaderboard"]
-exports.description = ["Displays the Leaderboard of a map.\n\n**Parameters:**\n\`link\` link a beatmap to get its leaderboard\n\`+(mod combination)\` get the leaderboard of that mod combination"]
+exports.description = ["Displays the Leaderboard of a map.\n\n**Parameters:**\n`link` link a beatmap to get its leaderboard\n`+(mod combination)` get the leaderboard of that mod combination"]
 exports.usage = [`lb +DTNF`]
 exports.category = ["osu"]
--- src/exports/leaderboard_export.js
+++ src/exports/leaderboard_export.js
@@ -1,156 +1,146 @@
-const {EmbedBuilder} = require("discord.js")
+const { EmbedBuilder } = require("discord.js")
 const fs = require("fs")
-const {v2, auth, mods, tools} = require("osu-api-extended")
-const {Beatmap, Calculator} = require("rosu-pp")
-const {Downloader, DownloadEntry} = require("osu-downloader")
+const { v2, auth, mods, tools } = require("osu-api-extended")
+const { Beatmap, Calculator } = require("rosu-pp")
+const { Downloader, DownloadEntry } = require("osu-downloader")
 
-async function LbSend(beatmapId, scores, pagenum) {
-  await auth.login(process.env.client_id, process.env.client_secret)
+async function LbSend(beatmapId, scores, pagenum, mapinfo) {
+	await auth.login(process.env.client_id, process.env.client_secret)
 
-  
-  const start = (pagenum - 1) * 5 + 1
-  const end = pagenum * 5
-  const numbers = []
-  for (let i = start; i <= end; i++) {
-    numbers.push(i)
-  }
-  const one = numbers[0] - 1
-  const two = numbers[1] - 1
-  const three = numbers[2] - 1
-  const four = numbers[3] - 1
-  const five = numbers[4] - 1
+	const start = (pagenum - 1) * 5 + 1
+	const end = pagenum * 5
+	const numbers = []
+	for (let i = start; i <= end; i++) {
+		numbers.push(i)
+	}
+	const one = numbers[0] - 1
+	const two = numbers[1] - 1
+	const three = numbers[2] - 1
+	const four = numbers[3] - 1
+	const five = numbers[4] - 1
 
-  try {
-    if (!fs.existsSync(`./osuFiles/${beatmapId}.osu`)) {
-      console.log("no file.")
-      const downloader = new Downloader({
-        rootPath: "./osuFiles",
+	try {
+		if (!fs.existsSync(`./osuFiles/${beatmapId}.osu`)) {
+			console.log("no file.")
+			const downloader = new Downloader({
+				rootPath: "./osuFiles",
 
-        filesPerSecond: 0,
-      })
-      downloader.addSingleEntry(beatmapId)
-      await downloader.downloadSingle()
-    }
+				filesPerSecond: 0,
+			})
+			downloader.addSingleEntry(beatmapId)
+			await downloader.downloadSingle()
+		}
 
-    let map = new Beatmap({path: `./osuFiles/${beatmapId}.osu`})
-    const mapinfo = await v2.beatmap.diff(beatmapId)
+		let map = new Beatmap({ path: `./osuFiles/${beatmapId}.osu` })
 
-    const grades = {
-      A: "<:A_:1057763284327080036>",
-      B: "<:B_:1057763286097076405>",
-      C: "<:C_:1057763287565086790>",
-      D: "<:D_:1057763289121173554>",
-      F: "<:F_:1057763290484318360>",
-      S: "<:S_:1057763291998474283>",
-      SH: "<:SH_:1057763293491642568>",
-      X: "<:X_:1057763294707974215>",
-      XH: "<:XH_:1057763296717045891>",
-    }
+		const grades = {
+			A: "<:A_:1057763284327080036>",
+			B: "<:B_:1057763286097076405>",
+			C: "<:C_:1057763287565086790>",
+			D: "<:D_:1057763289121173554>",
+			F: "<:F_:1057763290484318360>",
+			S: "<:S_:1057763291998474283>",
+			SH: "<:SH_:1057763293491642568>",
+			X: "<:X_:1057763294707974215>",
+			XH: "<:XH_:1057763296717045891>",
+		}
 
-    const totalPageRaw = scores.scores.length / 5
-    const totalPage = Math.ceil(totalPageRaw)
+		const totalPage = Math.ceil(scores.scores.length / 5)
 
-    async function ScoreGet(score, num) {
-      const ModsRaw = score.mods.map((mod) => mod.acronym).join("")
-      let modsID = mods.id(ModsRaw)
-      if (ModsRaw != "") {
-        Mods1 = `\`+${ModsRaw}\``
-      } else {
-        Mods1 = ""
-        modsID = 0
-      }
+		async function ScoreGet(score, num) {
+			const ModsRaw = score.mods.map(mod => mod.acronym).join("")
+			let modsID = mods.id(ModsRaw)
+			if (ModsRaw != "") {
+				Mods1 = `\`+${ModsRaw}\``
+			} else {
+				Mods1 = ""
+				modsID = 0
+			}
 
-      // std
-      if (score.statistics.great === undefined) score.statistics.great = 0
-      if (score.statistics.ok === undefined) score.statistics.ok = 0
-      if (score.statistics.meh === undefined) score.statistics.meh = 0
-      if (score.statistics.miss === undefined) score.statistics.miss = 0
+			// std
+			if (score.statistics.great === undefined) score.statistics.great = 0
+			if (score.statistics.ok === undefined) score.statistics.ok = 0
+			if (score.statistics.meh === undefined) score.statistics.meh = 0
+			if (score.statistics.miss === undefined) score.statistics.miss = 0
 
-      // mania
-      if (score.statistics.good === undefined) score.statistics.good = 0 // katu
-      if (score.statistics.perfect === undefined) score.statistics.perfect = 0 // geki
+			// mania
+			if (score.statistics.perfect === undefined) score.statistics.perfect = 0 // geki
+			if (score.statistics.good === undefined) score.statistics.good = 0 // katu
 
-      const scoreParam = {
-        mode: 0,
-        mods: modsID,
-      }
+			let AccValues
+			if (mapinfo.mode_int == "0") AccValues = `{**${score.statistics.great}**/${score.statistics.ok}/${score.statistics.meh}/${score.statistics.miss}}`
+			if (mapinfo.mode_int == "1") AccValues = `{**${score.statistics.great}**/${score.statistics.ok}}/${score.statistics.miss}}`
+			if (mapinfo.mode_int == "2") AccValues = `{**${score.statistics.great}**/${score.statistics.ok}/${score.statistics.meh}/${score.statistics.miss}}`
+			if (mapinfo.mode_int == "3") AccValues = `{**${score.statistics.perfect}/${score.statistics.great}**/${score.statistics.good}/${score.statistics.ok}/${score.statistics.meh}/${score.statistics.miss}}`
 
-      const calc = new Calculator(scoreParam)
+			const scoreParam = {
+				mode: 0,
+				mods: modsID,
+			}
 
-      // ss pp
-      const maxAttrs = calc.performance(map)
+			const calc = new Calculator(scoreParam)
 
-      //normal pp
-      const CurAttrs = calc
-        .n100(score.statistics.ok)
-        .n300(score.statistics.great)
-        .n50(score.statistics.meh)
-        .nMisses(Number(score.statistics.miss))
-        .combo(score.max_combo)
-        .nGeki(score.statistics.perfect)
-        .nKatu(score.statistics.good)
-        .performance(map)
+			// ss pp
+			const maxAttrs = calc.performance(map)
 
-      let grade = score.rank
-      grade = grades[grade]
+			//normal pp
+			const CurAttrs = calc.n100(score.statistics.ok).n300(score.statistics.great).n50(score.statistics.meh).nMisses(Number(score.statistics.miss)).combo(score.max_combo).nGeki(score.statistics.perfect).nKatu(score.statistics.good).performance(map)
 
-      PP = `**${CurAttrs.pp.toFixed(2)}**/${maxAttrs.pp.toFixed(2)}PP`
+			let grade = score.rank
+			grade = grades[grade]
 
-      const date = new Date(score.ended_at)
-      const UnixDate = date.getTime() / 1000
+			PP = `**${CurAttrs.pp.toFixed(2)}**/${maxAttrs.pp.toFixed(2)}PP`
 
-      const first_row = `**${num + 1}.** ${grade} [**${
-        score.user.username
-      }**](https://osu.ppy.sh/users/${score.user.id}) (${(
-        score.accuracy * 100
-      ).toFixed(2)}%) • ${score.total_score.toLocaleString()} **${
-        score.statistics.miss
-      }**<:hit00:1061254490075955231>\n`
-      const second_row = `▹${PP} • [ **${score.max_combo}x**/${
-        maxAttrs.difficulty.maxCombo
-      }x ] ${Mods1} • [${CurAttrs.difficulty.stars.toFixed(2)}★]\n`
-      const third_row = `▹Score Set: <t:${UnixDate}:R>`
-      return `${first_row}${second_row}${third_row}`
-    }
+			const date = new Date(score.ended_at)
+			const UnixDate = date.getTime() / 1000
 
-    first_score = "**No scores found.**"
-    if (scores.scores[one])
-      first_score = `${await ScoreGet(scores.scores[one], one)}\n`
+			const first_row = `**${num + 1}.** ${grade} [**${score.user.username}**](https://osu.ppy.sh/users/${score.user.id}) (${(score.accuracy * 100).toFixed(2)}%) • ${score.total_score.toLocaleString()} **${score.statistics.miss}**<:hit00:1061254490075955231>\n`
+			const second_row = `▹${PP} ${Mods1} • **__[${CurAttrs.difficulty.stars.toFixed(2)}★]__**\n`
+			const third_row = `▹[ **${score.max_combo}x**/${maxAttrs.difficulty.maxCombo}x ] • ${AccValues}\n`
+			const fourth_row = `▹Score Set: <t:${UnixDate}:R>`
+			return `${first_row}${second_row}${third_row}${fourth_row}`
+		}
 
-    second_score = ""
-    if (scores.scores[two])
-      second_score = `${await ScoreGet(scores.scores[two], two)}\n`
+		first_score = "**No scores found.**"
+		if (scores.scores[one]) first_score = `${await ScoreGet(scores.scores[one], one)}\n`
+		else {
+			console.log(mapinfo)
+			console.log("page", pagenum)
+			console.log("totalPage", pagenum)
 
-    third_score = ""
-    if (scores.scores[three])
-      third_score = `${await ScoreGet(scores.scores[three], three)}\n`
+			if (pagenum > totalPage || pagenum < 0) {
+				const embed = new EmbedBuilder()
+					.setColor("Red")
+					.setTitle(`Error..`) // [${starRating.difficulty.starRating.toFixed(2)}★]
+					.setDescription(`**Please pick a page between \`1\` and \`${totalPage}\`**`)
+				return embed
+			}
+		}
 
-    fourth_score = ""
-    if (scores.scores[four])
-      fourth_score = `${await ScoreGet(scores.scores[four], four)}\n`
+		second_score = ""
+		if (scores.scores[two]) second_score = `${await ScoreGet(scores.scores[two], two)}\n`
 
-    fifth_score = ""
-    if (scores.scores[five])
-      fifth_score = `${await ScoreGet(scores.scores[five], five)}`
+		third_score = ""
+		if (scores.scores[three]) third_score = `${await ScoreGet(scores.scores[three], three)}\n`
 
-    const embed = new EmbedBuilder()
-      .setColor("Purple")
-      .setTitle(
-        `${mapinfo.beatmapset.artist} - ${mapinfo.beatmapset.title} [${mapinfo.version}]`
-      ) // [${starRating.difficulty.starRating.toFixed(2)}★]
-      .setURL(`https://osu.ppy.sh/b/${mapinfo.id}`)
-      .setDescription(
-        `${first_score}${second_score}${third_score}${fourth_score}${fifth_score}`
-      )
-      .setImage(
-        `https://assets.ppy.sh/beatmaps/${mapinfo.beatmapset_id}/covers/cover.jpg`
-      )
-      .setFooter({text: `Page: 1/${totalPage}`})
+		fourth_score = ""
+		if (scores.scores[four]) fourth_score = `${await ScoreGet(scores.scores[four], four)}\n`
 
-    return embed
-  } catch (err) {
-    console.log(err)
-  }
+		fifth_score = ""
+		if (scores.scores[five]) fifth_score = `${await ScoreGet(scores.scores[five], five)}`
+
+		const embed = new EmbedBuilder()
+			.setColor("Purple")
+			.setTitle(`${mapinfo.beatmapset.artist} - ${mapinfo.beatmapset.title} [${mapinfo.version}]`) // [${starRating.difficulty.starRating.toFixed(2)}★]
+			.setURL(`https://osu.ppy.sh/b/${mapinfo.id}`)
+			.setDescription(`${first_score}${second_score}${third_score}${fourth_score}${fifth_score}`)
+			.setImage(`https://assets.ppy.sh/beatmaps/${mapinfo.beatmapset_id}/covers/cover.jpg`)
+			.setFooter({ text: `Page: ${pagenum}/${totalPage}` })
+
+		return embed
+	} catch (err) {
+		console.log(err)
+	}
 }
 
-module.exports = {LbSend}
+module.exports = { LbSend }
