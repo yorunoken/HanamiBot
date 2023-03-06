84c763fb39ea5ec9765132908efe33b4797586a0
Yoru <m.f.fetvaci@hotmail.com>
2023-03-06 17:05:21 +0300

    fixed length not showing correctly

--- src/commands/osu/stream.js
+++ src/commands/osu/stream.js
@@ -62,6 +62,11 @@ exports.run = async (client, message, args, prefix) => {
 			let minutes = Math.floor(length / 60)
 			let seconds = (length % 60).toString().padStart(2, "0")
 
+			// stream length
+			let slength = (data.streams_length / mapValues.clockRate).toFixed(0)
+			let sminutes = Math.floor(slength / 60)
+			let sseconds = (slength % 60).toString().padStart(2, "0")
+
 			//ranked or not
 			let status = DiffValues.status.charAt(0).toUpperCase() + DiffValues.status.slice(1)
 
@@ -70,7 +75,7 @@ exports.run = async (client, message, args, prefix) => {
 				.setColor("Purple")
 				.setAuthor({ name: `Beatmap by ${DiffValues.beatmapset.creator}`, url: `https://osu.ppy.sh/users/${DiffValues.user_id}`, iconURL: `https://a.ppy.sh/${DiffValues.user_id}?1668890819.jpeg` })
 				.setTitle(`${DiffValues.beatmapset.artist} - ${DiffValues.beatmapset.title}`)
-				.setFields({ name: `${osuEmote} **[${DiffValues.version}]**`, value: `Stars: \`${PP100.difficulty.stars.toFixed(2)}★\` Mods: \`NM\` BPM: \`${mapValues.bpm.toFixed()}\`\nLength: \`${minutes}:${seconds}\` Max Combo: \`${PP100.difficulty.maxCombo.toLocaleString()}x\` Objects: \`${PP100.difficulty.nCircles + PP100.difficulty.nSliders + PP100.difficulty.nSpinners + PP100.difficulty.nFruits}\`\nAR: \`${mapValues.ar.toFixed(1)}\` OD: \`${mapValues.od.toFixed(1)}\` CS: \`${mapValues.cs.toFixed(2)}\` HP: \`${mapValues.hp.toFixed(1)}\`\nLongest Stream: \`${data.longest_stream}\` Stream Density: \`${data.streams_density}\`\nStreams Length: \`${data.streams_length}\` Spacing: \`${data.streams_spacing}\`` }, { name: "PP", value: `\`\`\`Acc | PP\n95%:  ${PP95.pp.toFixed(1)}\n97%:  ${PP97.pp.toFixed(1)}\n99%:  ${PP99.pp.toFixed(1)}\n100%: ${PP100.pp.toFixed(1)}\`\`\``, inline: true }, { name: "Links", value: `:notes:[Song Preview](https://b.ppy.sh/preview/${DiffValues.beatmapset_id}.mp3)\n🎬[Map Preview](https://osu.pages.dev/preview#${DiffValues.id})\n🖼️[Full Background](https://assets.ppy.sh/beatmaps/${DiffValues.beatmapset_id}/covers/raw.jpg)\n<:beatconnect:1075915329512931469>[Beatconnect](https://beatconnect.io/b/${DiffValues.beatmapset_id})\n<:kitsu:1075915745973776405>[Kitsu](https://kitsu.moe/d/${DiffValues.beatmapset_id})`, inline: true })
+				.setFields({ name: `${osuEmote} **[${DiffValues.version}]**`, value: `Stars: \`${PP100.difficulty.stars.toFixed(2)}★\` Mods: \`NM\` BPM: \`${mapValues.bpm.toFixed()}\`\nLength: \`${minutes}:${seconds}\` Max Combo: \`${PP100.difficulty.maxCombo.toLocaleString()}x\` Objects: \`${PP100.difficulty.nCircles + PP100.difficulty.nSliders + PP100.difficulty.nSpinners + PP100.difficulty.nFruits}\`\nAR: \`${mapValues.ar.toFixed(1)}\` OD: \`${mapValues.od.toFixed(1)}\` CS: \`${mapValues.cs.toFixed(2)}\` HP: \`${mapValues.hp.toFixed(1)}\`\nLongest Stream: \`${data.longest_stream}\` Stream Density: \`${data.streams_density}\`\nStreams Length: \`${sminutes}:${sseconds}\` Spacing: \`${data.streams_spacing}\`` }, { name: "PP", value: `\`\`\`Acc | PP\n95%:  ${PP95.pp.toFixed(1)}\n97%:  ${PP97.pp.toFixed(1)}\n99%:  ${PP99.pp.toFixed(1)}\n100%: ${PP100.pp.toFixed(1)}\`\`\``, inline: true }, { name: "Links", value: `:notes:[Song Preview](https://b.ppy.sh/preview/${DiffValues.beatmapset_id}.mp3)\n🎬[Map Preview](https://osu.pages.dev/preview#${DiffValues.id})\n🖼️[Full Background](https://assets.ppy.sh/beatmaps/${DiffValues.beatmapset_id}/covers/raw.jpg)\n<:beatconnect:1075915329512931469>[Beatconnect](https://beatconnect.io/b/${DiffValues.beatmapset_id})\n<:kitsu:1075915745973776405>[Kitsu](https://kitsu.moe/d/${DiffValues.beatmapset_id})`, inline: true })
 				.setURL(`https://osu.ppy.sh/b/${DiffValues.id}`)
 				.setImage(`https://assets.ppy.sh/beatmaps/${DiffValues.beatmapset_id}/covers/cover.jpg`)
 				.setFooter({ text: `${status} | ${DiffValues.beatmapset.favourite_count} ♥` })
@@ -211,12 +216,12 @@ exports.run = async (client, message, args, prefix) => {
 		longest_min = {
 			property: "longest_stream",
 			operator: "minimum",
-			value: Number(argValues["longest"]) - 5,
+			value: Number(argValues["longest"]) - 25,
 		}
 		longest_max = {
 			property: "longest_stream",
 			operator: "maximum",
-			value: Number(argValues["longest"]) + 5,
+			value: Number(argValues["longest"]) + 25,
 		}
 	}
 
@@ -265,24 +270,24 @@ exports.run = async (client, message, args, prefix) => {
 		streamlength_min = {
 			property: "streams_length",
 			operator: "minimum",
-			value: Number(argValues["streamlength"]) - 4,
+			value: Number(argValues["streamlength"]) - 10,
 		}
 		streamlength_max = {
 			property: "streams_length",
 			operator: "maximum",
-			value: Number(argValues["streamlength"]) + 4,
+			value: Number(argValues["streamlength"]) + 10,
 		}
 	}
 
 	let spacing_min = defaultmin
 	let spacing_max = defaultmax
 	if (argValues["spacing"]) {
-		streamlength_min = {
+		spacing_min = {
 			property: "streams_spacing",
 			operator: "minimum",
 			value: Number(argValues["spacing"]) - 0.2,
 		}
-		streamlength_max = {
+		spacing_max = {
 			property: "streams_spacing",
 			operator: "maximum",
 			value: Number(argValues["spacing"]) + 0.2,
@@ -307,6 +312,6 @@ exports.run = async (client, message, args, prefix) => {
 
 exports.name = "stream"
 exports.aliases = ["stream", "recc", "recommend", "streamrec", "streamrecommend", "rec"]
-exports.description = ["Recommends you a stream map by given parameters\n\n**Parameters:**\n`od=(number)` recommends you a map by its od (the od will have a value between -0.2 and +0.2)\n`ar=(number)` recommends you a map by its ar (the ar will have a value between -0.2 and +0.2)\n`cs=(number)` recommends you a map by its cs (the cs will have a value between -0.2 and +0.2)\n`bpm=(number)` recommends you a map by its bpm (the bpm will have a value between -5 and +5)\n`star=(number)` recommends you a map by its stars (the stars will have a value between -0.1 and +0.1)\n`length=(number)` recommends you a map by its length, in seconds (the length will have a value between -15 and +15)\n`longest=(number)` recommends you a map by its longest stream (the longest stream will have a value between -5 and +5)\n`pp=(number)` recommends you a map by its SS pp (the SS pp will have a value between -10 and +10)\n`status=(string)` recommends you a map by its ranked status. (ranked, loved, unranked)\n`density=(number)` recommends you a map by its stream density (the density will have a value between -0.03 and +0.03)\n`streamlength=(number)` recommends you a map by its streams length (the streams length will have a value between -4 and +4)\n`spacing=(number)` recommends you a map by its stream spacing (the spacing will have a value between -0.2 and +0.2)\n\n[Credits to Sombrax79 for making the website](https://ost.sombrax79.org/)"]
+exports.description = ["Recommends you a stream map by given parameters\n\n**Parameters:**\n`od=(number)` recommends you a map by its od (the od will have a value between -0.2 and +0.2)\n`ar=(number)` recommends you a map by its ar (the ar will have a value between -0.2 and +0.2)\n`cs=(number)` recommends you a map by its cs (the cs will have a value between -0.2 and +0.2)\n`bpm=(number)` recommends you a map by its bpm (the bpm will have a value between -5 and +5)\n`star=(number)` recommends you a map by its stars (the stars will have a value between -0.1 and +0.1)\n`length=(number)` recommends you a map by its length, in seconds (the length will have a value between -15 and +15)\n`longest=(number)` recommends you a map by its longest stream (the longest stream will have a value between -25 and +25)\n`pp=(number)` recommends you a map by its SS pp (the SS pp will have a value between -10 and +10)\n`status=(string)` recommends you a map by its ranked status. (ranked, loved, unranked)\n`density=(number)` recommends you a map by its stream density (the density will have a value between -0.03 and +0.03)\n`streamlength=(number)` recommends you a map by its streams length (the streams length will have a value between -10 and +10)\n`spacing=(number)` recommends you a map by its stream spacing (the spacing will have a value between -0.2 and +0.2)\n\n[Credits to Sombrax79 for making the website](https://ost.sombrax79.org/)"]
 exports.usage = [`stream bpm=200 star=6.3 od=9.6`]
 exports.category = ["osu"]
