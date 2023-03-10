75204f7748f7ae729cd72c5d879cbc5d675bb7c2
Yoru <m.f.fetvaci@hotmail.com>
2023-03-10 16:30:51 +0300

    added stats command

---
+++ src/commands/osu_stats/statstaiko.js
@@ -0,0 +1,103 @@
+const fs = require("fs")
+const { v2, auth } = require("osu-api-extended")
+const { EmbedBuilder } = require("discord.js")
+
+// importing GetRecent
+const { GetUserTop100Stats } = require("../../exports/stats_export.js")
+
+module.exports.run = async (client, message, args, prefix) => {
+	await message.channel.sendTyping()
+
+	fs.readFile("./user-data.json", async (error, data) => {
+		if (error) {
+			console.log(error)
+			return
+		}
+		const userData = JSON.parse(data)
+		let userargs
+
+		let mode = "taiko"
+		let RuleSetId = 1
+
+		if (message.mentions.users.size > 0) {
+			const mentionedUser = message.mentions.users.first()
+			try {
+				if (mentionedUser) {
+					if (message.content.includes(`<@${mentionedUser.id}>`)) {
+						userargs = userData[mentionedUser.id].osuUsername
+					} else {
+						userargs = userData[message.author.id].osuUsername
+					}
+				}
+			} catch (err) {
+				console.error(err)
+				if (mentionedUser) {
+					if (message.content.includes(`<@${mentionedUser.id}>`)) {
+						try {
+							userargs = userData[mentionedUser.id].osuUsername
+						} catch (err) {
+							message.reply(`No osu! user found for ${mentionedUser.tag}`)
+						}
+					} else {
+						try {
+							userargs = userData[message.author.id].osuUsername
+						} catch (err) {
+							message.reply(`Set your osu! username by typing "${prefix}link **your username**"`)
+						}
+					}
+				}
+				return
+			}
+		} else {
+			if (args[0] === undefined) {
+				try {
+					userargs = userData[message.author.id].osuUsername
+				} catch (err) {
+					console.error(err)
+					message.reply(`Set your osu! username by typing "${prefix}link **your username**"`)
+					return
+				}
+			} else {
+				let string = args.join(" ").match(/"(.*?)"/)
+				if (string) {
+					userargs = string[1]
+				} else {
+					userargs = args[0]
+				}
+			}
+		}
+
+		if (userargs.length === 0) {
+			try {
+				userargs = userData[message.author.id].osuUsername
+			} catch (err) {
+				message.reply(`Set your osu! username by typing "${prefix}link **your username**"`)
+			}
+		}
+
+		//log in
+		await auth.login(process.env.client_id, process.env.client_secret)
+		const user = await v2.user.details(userargs, mode)
+		if (user.id === undefined) {
+			message.channel.send(`**The player, \`${userargs}\` does not exist**`)
+			return
+		}
+
+		const plays = await v2.user.scores.category(user.id, "best", {
+			mode: mode,
+			limit: "100",
+			offset: "0",
+		})
+
+		const WaitMesasge = await message.channel.send({ embeds: [new EmbedBuilder().setColor("Purple").setTitle("Calculating...").setDescription("Please sit still while I'm calculating your plays, this may take a while if it's your first time using this command.")] })
+
+		const Stats = await GetUserTop100Stats(user, plays, RuleSetId, mode)
+		WaitMesasge.delete()
+		message.channel.send({ embeds: [Stats] })
+	})
+}
+exports.name = ["statstaiko"]
+exports.aliases = ["statstaiko", "stattaiko", "statst", "statt", "tstats", "tstat", "taikostats"]
+exports.description = ["Displays user's osu!taiko stats\n\n**Parameters:**\n`username`"]
+exports.usage = [`statst YoruNoKen`]
+exports.category = ["osu"]
---
+++ src/exports/stats_export.js
@@ -0,0 +1,149 @@
+const { Beatmap, Calculator } = require("rosu-pp")
+const { Downloader, DownloadEntry } = require("osu-downloader")
+const { mods } = require("osu-api-extended")
+const { EmbedBuilder } = require("discord.js")
+const fs = require("fs")
+const Table = require("easy-table")
+
+async function GetUserTop100Stats(user, tops, ruleset, mode) {
+	try {
+		global_rank = user.statistics.global_rank.toLocaleString()
+		country_rank = user.statistics.country_rank.toLocaleString()
+		pp = user.statistics.pp.toLocaleString()
+	} catch (err) {
+		global_rank = "0"
+		country_rank = "0"
+		pp = "0"
+	}
+
+	let Attrs = []
+	let MapDifficulty = []
+	let PlayedMods = []
+	let RawMappers = []
+	for (let i = 0; i < tops.length; i++) {
+		const score = tops[i]
+
+		if (!fs.existsSync(`./osuFiles/${score.beatmap.id}.osu`)) {
+			console.log(`no file, ${i}`)
+			const downloader = new Downloader({
+				rootPath: "./osuFiles",
+
+				filesPerSecond: 0,
+			})
+
+			downloader.addSingleEntry(score.beatmap.id)
+			await downloader.downloadSingle()
+		}
+		let modsID = mods.id(score.mods.join(""))
+		if (!score.mods.join("").length) modsID = 0
+
+		let scoreParam = {
+			mode: ruleset,
+			mods: modsID,
+		}
+
+		let map = new Beatmap({ path: `./osuFiles/${score.beatmap.id}.osu` })
+
+		const pp = new Calculator(scoreParam).n100(score.statistics.count_100).n300(score.statistics.count_300).n50(score.statistics.count_50).nMisses(Number(score.statistics.count_miss)).combo(score.max_combo).nGeki(score.statistics.count_geki).nKatu(score.statistics.count_katu).performance(map)
+		const mapValues = new Calculator(scoreParam).mapAttributes(map)
+
+		let UsedMods = score.mods.join("")
+		if (UsedMods.length == 0) UsedMods = "NM"
+
+		Attrs.push(pp)
+		MapDifficulty.push(mapValues)
+		PlayedMods.push(UsedMods)
+		RawMappers.push(score.beatmapset.creator)
+	}
+
+	const acc = tops.map(x => x.accuracy * 100)
+	const combo = tops.map(x => x.max_combo)
+	const miss = tops.map(x => x.statistics.count_miss)
+	const pps = tops.map(x => x.pp)
+	const star = Attrs.map(x => x.difficulty.stars)
+	const bpm = MapDifficulty.map(x => x.bpm)
+	const ar = MapDifficulty.map(x => x.ar)
+	const od = MapDifficulty.map(x => x.od)
+	const cs = MapDifficulty.map(x => x.cs)
+	const hp = MapDifficulty.map(x => x.hp)
+
+	const modCounts = []
+
+	for (let mod of PlayedMods) {
+		let index = modCounts.findIndex(element => element.mod === mod)
+		if (index !== -1) {
+			modCounts[index].count++
+		} else {
+			modCounts.push({ mod: mod, count: 1 })
+		}
+	}
+
+	modCounts.sort((a, b) => b.count - a.count)
+	const FirstSixMods = modCounts.slice(0, 6)
+
+	let FavMods = ""
+	for (let modCount of FirstSixMods) {
+		FavMods += `\`${modCount.mod}: ${modCount.count}\`\n`
+	}
+
+	const MappersCount = []
+
+	for (let mapper of RawMappers) {
+		let index = MappersCount.findIndex(element => element.mapper === mapper)
+		if (index !== -1) {
+			MappersCount[index].count++
+		} else {
+			MappersCount.push({ mapper: mapper, count: 1 })
+		}
+	}
+
+	MappersCount.sort((a, b) => b.count - a.count)
+
+	const FirstSixMappers = MappersCount.slice(0, 6)
+
+	let FavMapper = ""
+	for (let Mapper of FirstSixMappers) {
+		FavMapper += `\`${Mapper.mapper}: ${Mapper.count}\`\n`
+	}
+
+	function GetMinAvgMax(array) {
+		const avg = Number((array.reduce((Array, num) => Array + parseFloat(num), 0) / array.length).toFixed(2))
+		const min = Number(Math.min.apply(null, array).toFixed(2))
+		const max = Number(Math.max.apply(null, array).toFixed(2))
+		return { avg, min, max }
+	}
+
+	const data = [
+		{ name: "Stars", min: GetMinAvgMax(star).min, avg: GetMinAvgMax(star).avg, max: GetMinAvgMax(star).max },
+		{ name: "Accuracy", min: GetMinAvgMax(acc).min, avg: GetMinAvgMax(acc).avg, max: GetMinAvgMax(acc).max },
+		{ name: "Combo", min: GetMinAvgMax(combo).min.toFixed(), avg: GetMinAvgMax(combo).avg.toFixed(), max: GetMinAvgMax(combo).max.toFixed() },
+		{ name: "PP", min: GetMinAvgMax(pps).min, avg: GetMinAvgMax(pps).avg, max: GetMinAvgMax(pps).max },
+		{ name: "Miss", min: GetMinAvgMax(miss).min, avg: GetMinAvgMax(miss).avg, max: GetMinAvgMax(miss).max },
+		{ name: "BPM", min: GetMinAvgMax(bpm).min, avg: GetMinAvgMax(bpm).avg, max: GetMinAvgMax(bpm).max },
+		{ name: "AR", min: GetMinAvgMax(ar).min, avg: GetMinAvgMax(ar).avg, max: GetMinAvgMax(ar).max },
+		{ name: "OD", min: GetMinAvgMax(od).min, avg: GetMinAvgMax(od).avg, max: GetMinAvgMax(od).max },
+		{ name: "CS", min: GetMinAvgMax(cs).min, avg: GetMinAvgMax(cs).avg, max: GetMinAvgMax(cs).max },
+		{ name: "HP", min: GetMinAvgMax(hp).min, avg: GetMinAvgMax(hp).avg, max: GetMinAvgMax(hp).max },
+	]
+
+	const t = new Table()
+	data.forEach(function (Skills) {
+		t.cell("", Skills.name)
+		t.cell("Minimum", Skills.min)
+		t.cell("Average", Skills.avg)
+		t.cell("Maximum", Skills.max)
+		t.newRow()
+	})
+
+	return new EmbedBuilder()
+		.setColor("Purple")
+		.setAuthor({
+			name: `${user.username}: ${pp}pp (#${global_rank} ${user.country.code}#${country_rank})`,
+			iconURL: `https://osuflags.omkserver.nl/${user.country_code}-256.png`,
+			url: `https://osu.ppy.sh/users/${user.id}/${mode}`,
+		})
+		.setThumbnail(user.avatar_url)
+		.setFields({ name: `Performance Spread:`, value: `\`\`\`${t.toString()}\`\`\``, inline: false }, { name: `Top Mod Combos:`, value: `${FavMods}`, inline: true }, { name: `Top Mappers:`, value: `${FavMapper}`, inline: true })
+}
+
+module.exports = { GetUserTop100Stats }
