df52da9c3ac6251a3e0cc2d451e33a22c9cda076
Yoru <m.f.fetvaci@hotmail.com>
2023-03-18 16:50:06 +0300

    added recentplayStats

---
+++ src/commands/osu_recentstats/playstats.js
@@ -0,0 +1,48 @@
+const fs = require("fs")
+const { v2, auth } = require("osu-api-extended")
+const { EmbedBuilder } = require("discord.js")
+
+// importing GetRecent
+const { PlayStats } = require("../../exports/playstats_export.js")
+const { FindUserargs } = require("../../exports/finduserargs_export.js")
+
+module.exports.run = async (client, message, args, prefix) => {
+	await message.channel.sendTyping()
+
+	fs.readFile("./user-data.json", async (error, data) => {
+		if (error) {
+			console.log(error)
+			return
+		}
+		const userData = JSON.parse(data)
+		var server = "bancho"
+
+		let mode = "osu"
+		let RuleSetId = 0
+
+		var userargs = await FindUserargs(message, args, server, prefix)
+
+		if (userargs.length === 0) {
+			try {
+				userargs = userData[message.author.id].BanchoUserId
+			} catch (err) {
+				message.reply(`Set your osu! username by typing "${prefix}link **your username**"`)
+			}
+		}
+
+		//log in
+		await auth.login(process.env.client_id, process.env.client_secret)
+		const user = await v2.user.details(userargs, mode)
+		if (user.id === undefined) {
+			message.reply({ embeds: [new EmbedBuilder().setColor("Purple").setDescription(`**The player \`${userargs}\` does not exist**`)] })
+			return
+		}
+
+		message.channel.send({ embeds: [await PlayStats(user, RuleSetId, mode)] })
+	})
+}
+exports.name = ["playstats"]
+exports.aliases = ["playstats", "playstats", "ps", "pstat"]
+exports.description = ["Displays user's osu!standard stats based on recent plays\n\n**Parameters:**\n`username`"]
+exports.usage = [`ps YoruNoKen`]
+exports.category = ["osu"]
---
+++ src/exports/playstats_export.js
@@ -0,0 +1,125 @@
+const fs = require("fs")
+const { EmbedBuilder } = require("discord.js")
+const Table = require("easy-table")
+
+async function PlayStats(user, RuleSetId, mode) {
+	return new Promise((resolve, reject) => {
+		fs.readFile("./user-recent.json", async (error, data) => {
+			if (error) {
+				console.log(error)
+				return
+			}
+			var Recents = JSON.parse(data)
+
+			var userId = user.id
+			var scores = Recents[userId]
+
+			global_rank = user.statistics.global_rank.toLocaleString() || "-"
+			country_rank = user.statistics.country_rank.toLocaleString() || "-"
+			pp = user.statistics.pp.toLocaleString()
+
+			if (scores == undefined) {
+				const embed = new EmbedBuilder()
+					.setColor("Purple")
+					.setAuthor({
+						name: `${user.username}: ${pp}pp (#${global_rank} ${user.country.code}#${country_rank})`,
+						iconURL: `https://osu.ppy.sh/images/flags/${user.country_code}.png`,
+						url: `https://osu.ppy.sh/users/${user.id}/${mode}`,
+					})
+					.setThumbnail(user.avatar_url)
+					.setFields({ name: `Statistics:`, value: `\`\`\`${user.username} was not rs'd any scores using this bot.\`\`\``, inline: false })
+				resolve(embed)
+				return
+			}
+
+			function FindPattern(Array, Acc) {
+				let avg = (Array.reduce((a, b) => a + b) / Array.length).toFixed(2)
+				let min = Number(Math.min.apply(null, Array).toFixed(2))
+				let max = Number(Math.max.apply(null, Array).toFixed(2))
+				if (Acc) {
+					avg = ((Array.reduce((a, b) => a + b) / Array.length) * 100).toFixed(2)
+					min = (Number(Math.min.apply(null, Array)) * 100).toFixed(2)
+					max = (Number(Math.max.apply(null, Array)) * 100).toFixed(2)
+				}
+
+				return { avg, min, max }
+			}
+
+			const Stars = FindPattern(
+				scores.scores.map(score => score.score.StarRating),
+				false,
+			)
+			const Miss = FindPattern(
+				scores.scores.map(score => score.score.statistics.count_miss),
+				false,
+			)
+			const Count300 = FindPattern(
+				scores.scores.map(score => score.score.statistics.count_300),
+				false,
+			)
+			const Count100 = FindPattern(
+				scores.scores.map(score => score.score.statistics.count_100),
+				false,
+			)
+			const Count50 = FindPattern(
+				scores.scores.map(score => score.score.statistics.count_50),
+				false,
+			)
+			const bpm = FindPattern(
+				scores.scores.map(score => score.score.bpm),
+				false,
+			)
+			const Acc = FindPattern(
+				scores.scores.map(score => score.score.accuracy),
+				true,
+			)
+			const Combo = FindPattern(
+				scores.scores.map(score => score.score.max_combo),
+				false,
+			)
+			const PPCur = FindPattern(
+				scores.scores.map(score => score.score.CurPP),
+				false,
+			)
+			const PPFix = FindPattern(
+				scores.scores.map(score => score.score.FixPP),
+				false,
+			)
+			// const PPSS = FindPattern(scores.scores.map(score => score.score.SSPP))
+
+			const DataForTable = [
+				{ name: "Stars", min: Stars.min, avg: Stars.avg, max: Stars.max },
+				{ name: "Accuracy", min: Acc.min, avg: Acc.avg, max: Acc.max },
+				{ name: "Combo", min: Combo.min, avg: Combo.avg, max: Combo.max },
+				{ name: "PP", min: PPCur.min, avg: PPCur.avg, max: PPCur.max },
+				{ name: "PP FC", min: PPFix.min, avg: PPFix.avg, max: PPFix.max },
+				{ name: "BPM", min: bpm.min, avg: bpm.avg, max: bpm.max },
+				{ name: "n300", min: Count300.min, avg: Count300.avg, max: Count300.max },
+				{ name: "n100", min: Count100.min, avg: Count100.avg, max: Count100.max },
+				{ name: "n50", min: Count50.min, avg: Count50.avg, max: Count50.max },
+				{ name: "Miss", min: Miss.min, avg: Miss.avg, max: Miss.max },
+			]
+
+			t = new Table()
+			DataForTable.forEach(function (Skills) {
+				t.cell("", Skills.name)
+				t.cell("Minimum", Skills.min)
+				t.cell("Average", Skills.avg)
+				t.cell("Maximum", Skills.max)
+				t.newRow()
+			})
+			const embed = new EmbedBuilder()
+				.setColor("Purple")
+				.setAuthor({
+					name: `${user.username}: ${pp}pp (#${global_rank} ${user.country.code}#${country_rank})`,
+					iconURL: `https://osu.ppy.sh/images/flags/${user.country_code}.png`,
+					url: `https://osu.ppy.sh/users/${user.id}/${mode}`,
+				})
+				.setThumbnail(user.avatar_url)
+				.setFields({ name: `Statistics:`, value: `\`\`\`${t.toString()}\`\`\``, inline: false })
+			resolve(embed)
+		})
+	})
+}
+
+module.exports = { PlayStats }
