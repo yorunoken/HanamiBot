99b8b03512e90709d22930608259eb06a0ff5477
Yoru <69751661+YoruNoKen@users.noreply.github.com>
2023-06-11 03:18:34 +0300

    sfddf

--- src/commands/prefix/owner/terminate.js
+++ src/commands/prefix/owner/terminate.js
@@ -2,12 +2,12 @@ async function run(message, client) {
   if (message.author.id !== "372343076578131968") {
     return message.reply("You must be the owner to use this command.");
   }
-  message.reply("Terminated instance.").then(() => client.destroy());
+  message.reply("Terminated.").then(() => client.destroy());
 }
 
 module.exports = {
-  name: "avatar",
-  aliases: ["avatar"],
+  name: "terminate",
+  aliases: ["terminate"],
   cooldown: 5000,
   run: async ({ message, client }) => {
     await run(message, client);
