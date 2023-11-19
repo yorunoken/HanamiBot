266d846d04190f5c3cecf6ba0cec6cf9d4cba2ec
Yoru <m.f.fetvaci@hotmail.com>
2023-11-19 13:39:27 +0000

    added `shout` and `feedback` commands

--- src/Helpers/help.ts
+++ src/Helpers/help.ts
@@ -26,11 +26,12 @@ function helpMenu(options: any, commands: any) {
   
   embed.addFields({ name: "general", value: "```" + Object.values(categories.general).map((element: any) => element.name).join(", ") + "```" , inline: true });
   embed.addFields({ name: "osu", value: "```" + Object.values(categories.osu).map((element: any) => element.name).join(", ") + "```", inline: true });
+  embed.addFields({ name: "owner", value: "```" + Object.values(categories.owner).map((element: any) => element.name).join(", ") + "```", inline: true });
   options.reply({ embeds: [embed] });
 }
 
 function commandHelp(options: any, name: string, commands: any) {
-  const command = Object.values(commands).find((cmd: any) => cmd.aliases.includes(name) || cmd.name.includes(name)) as ModuleReturn | undefined;
+  const command = Object.values(commands).find((cmd: any) => cmd.aliases.some((alias: string) => alias.toLowerCase() === name.toLowerCase()) || cmd.name.toLowerCase() === name.toLowerCase()) as ModuleReturn | undefined;
   if (!command) {
     options.reply(`The command with the name (or alias) \`${name}\` was not found.`);
     return;
---
+++ src/SlashCommands/general/feedback.ts
@@ -0,0 +1,17 @@
+import { ChatInputCommandInteraction, Client, EmbedBuilder, TextChannel } from "discord.js";
+
+export async function run({ interaction, client }: { interaction: ChatInputCommandInteraction; client: Client }) {
+  await interaction.deferReply();
+
+  const channel = (await client.channels.fetch(Bun.env.DEV_CHANNEL_ID as string)) as TextChannel;
+  if (!channel) {
+    return interaction.editReply("This channel doesn't exist.");
+  }
+
+  const sender = interaction.user;
+  const input = interaction.options.getString("feedback")!;
+  const type = interaction.options.getString("type")!;
+
+  channel.send({ embeds: [new EmbedBuilder().setAuthor({ name: `Feedback from ${sender.username}`, iconURL: `${sender.avatarURL}` }).addFields({ name: `Type: ${type}`, value: input })] });
+}
+export { data } from "../data/feedback";
