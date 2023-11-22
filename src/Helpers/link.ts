0f71a83158a4c97a7bc6b42f7d840f9777c36df1
Yoru <m.f.fetvaci@hotmail.com>
2023-11-22 13:05:14 +0000

    names with spaces in them now won't merge in `link.ts`

--- src/Helpers/link.ts
+++ src/Helpers/link.ts
@@ -1,28 +1,38 @@
-import { ChatInputCommandInteraction, EmbedBuilder, Message } from "discord.js";
-import { insertData, getUser, Interactionhandler } from "../utils";
+import { ChatInputCommandInteraction, EmbedBuilder, Message, User } from "discord.js";
+import { insertData, getUser, Interactionhandler, getUsernameFromArgs } from "../utils";
 import { v2 } from "osu-api-extended";
 
 export async function start(interaction: ChatInputCommandInteraction | Message, args?: string[]) {
-  const options = Interactionhandler(interaction, args)
-  const username = options.userArgs?.join("")!;
-  const author = options.author;
+  const options = Interactionhandler(interaction, args);
+  const { reply } = options;
 
-  const user = await v2.user.details(username, "osu");
+  const username = options.userArgs;
+  if (username.length === 0) {
+    return reply("Please provide a username");
+  }
+
+  const userOptions = getUsernameFromArgs({} as User, username);
+  if (userOptions?.user.status === false) {
+    return reply('Something went wrong, try wrapping the username in quotes (")');
+  }
+
+  const author = options.author;
+  const user = await v2.user.details(userOptions?.user, "osu");
   if (!user.id) {
-    return options.reply(`The user \`${username}\` does not exist in Bancho.`);
+    return options.reply(`The user \`${userOptions?.user}\` does not exist in Bancho.`);
   }
 
   let currentDocs = await getUser(author.id);
   if (!currentDocs) {
     currentDocs = {};
     currentDocs.data = JSON.stringify({ banchoId: user.id });
-    await insertData({ table: "users", id: author.id, data: currentDocs.data });
+    insertData({ table: "users", id: author.id, data: currentDocs.data });
   }
   let data = JSON.parse(currentDocs.data);
   data.banchoId = user.id;
 
-  await insertData({ table: "users", id: author.id, data: JSON.stringify(data) });
+  insertData({ table: "users", id: author.id, data: JSON.stringify(data) });
 
   const embed = new EmbedBuilder().setColor("Green").setTitle(`Success!`).setDescription(`Successfully linked your Discord account (<@${author.id}>) to osu! user ${user.username}`).setThumbnail(user.avatar_url);
-  await options.reply({ embeds: [embed] });
+  await reply({ embeds: [embed] });
 }
