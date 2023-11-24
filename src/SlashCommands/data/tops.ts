6893bd32b4dfb58830cff36097c09abb7381ea33
Yoru <m.f.fetvaci@hotmail.com>
2023-11-24 15:31:16 +0000

    fix tops

--- src/SlashCommands/data/tops.ts
+++ src/SlashCommands/data/tops.ts
@@ -4,5 +4,5 @@ export const data = new SlashCommandBuilder()
   .setDescription("Displays a user's top scores")
   .addStringOption((option) => option.setName("user").setDescription("Specify a username. (or tag someone)"))
   .addStringOption((option) => option.setName("mode").setDescription("Select an osu! mode").addChoices({ name: "standard", value: "osu" }, { name: "mania", value: "mania" }, { name: "taiko", value: "taiko" }, { name: "catch", value: "fruits" }))
-  .addIntegerOption((option) => option.setName("page").setDescription("The page of the top plays.").setMinValue(1).setMaxValue(10))
+  .addIntegerOption((option) => option.setName("page").setDescription("The page of the top plays.").setMinValue(1).setMaxValue(20))
   .addIntegerOption((option) => option.setName("index").setDescription("The index of the top plays.").setMinValue(1).setMaxValue(100));
