1b296fb7b55b3467944888b5e653d20cc644b784
YoruNoKen <m.f.fetvaci@hotmail.com>
2023-11-25 03:17:42 +0300

    will continue coding tomorrow

--- src/Helpers/nochoke.ts
+++ src/Helpers/nochoke.ts
@@ -1,8 +1,11 @@
-import { Message, ChatInputCommandInteraction } from "discord.js";
+import { Message, ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
 import { MyClient } from "../classes";
 import { osuModes } from "../types";
 import { getUser } from "../functions";
-import { Interactionhandler, getUsernameFromArgs } from "../utils";
+import { response as UserResponse } from "osu-api-extended/dist/types/v2_user_details";
+
+import { response as ScoreResponse } from "osu-api-extended/dist/types/v2_scores_user_category";
+import { Interactionhandler, downloadMap, getMapsInBulk, getUsernameFromArgs, insertDataBulk } from "../utils";
 import { v2 } from "osu-api-extended";
 
 export async function start({ interaction, args, mode, client }: { interaction: Message | ChatInputCommandInteraction; args?: string[]; mode?: osuModes; client: MyClient }) {
@@ -11,7 +14,7 @@ export async function start({ interaction, args, mode, client }: { interaction: 
   mode = (mode ?? interactionOptions.mode) as osuModes;
 
   const options = getUsernameFromArgs(author, userArgs);
-  if (!options || options.user?.status) {
+  if (!options || options.user?.status === false) {
     return reply(options?.user.message ?? "Something went wrong.");
   }
 
@@ -21,10 +24,38 @@ export async function start({ interaction, args, mode, client }: { interaction: 
   }
 
   let plays = await v2.scores.user.category(user.id, "best", {
-    limit: "100",
+    limit: "5",
     mode,
   });
   plays = options.flags.rev ? plays.sort((a, b) => Number(a.pp) - Number(b.pp)) : plays;
+  getNoChoke(plays, user, reply);
+}
+
+async function getNoChoke(plays: ScoreResponse[], user: UserResponse, reply: (options: any) => Promise<Message<boolean>>) {
+  const files = getFiles(plays, user, reply);
+}
+
+async function getFiles(plays: ScoreResponse[], user: UserResponse, reply: (options: any) => Promise<Message<boolean>>) {
+  const mapIds = plays.map((play) => play.beatmap.id);
+  let mapsInBulk = getMapsInBulk(mapIds);
+  const missingMapIds = mapIds.filter((id) => !mapsInBulk.some((map: any) => map.id === id));
+  if (missingMapIds.length > 0) {
+    const message = await reply({ embeds: [new EmbedBuilder().setTitle("Warning!").setDescription(`\`${missingMapIds.length}\`of ${user.username}'s plays are not in the bot's database. Please wait while the bot is download your maps.`).setColor("Red")] });
+    const data = (await downloadMap(mapIds)).map((map: any) => ({ id: map.id, data: map.contents }));
+    insertDataBulk({
+      table: "maps",
+      object: data,
+    });
+    data.forEach((map: any) => {
+      mapsInBulk = [...mapsInBulk, map];
+    });
+    message.edit({ embeds: [new EmbedBuilder().setTitle("Success").setDescription("Maps have been downloaded, setting up embed.").setColor("Green")] });
+  }
+
+  return mapsInBulk.reduce((acc: any, { id, data }: { id: number; data: string }) => {
+    acc[id] = data;
+    return acc;
+  }, {});
 }
 
 // const userDetails = getUser({ user, mode });
