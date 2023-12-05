9793c872cf015790ac5d405eee03288c865e4d4f
YoruNoKen <m.f.fetvaci@hotmail.com>
2023-12-05 23:11:38 +0300

    updated help to include new features

--- src/Helpers/help.ts
+++ src/Helpers/help.ts
@@ -1,7 +1,7 @@
-import { ChatInputCommandInteraction, EmbedBuilder, Message } from "discord.js";
+import { ChatInputCommandInteraction, Client, EmbedBuilder, Message } from "discord.js";
 import fs from "fs";
 import { Locales, ModuleReturn } from "../Structure/index";
-import { Interactionhandler } from "../utils";
+import { getWholeDb, Interactionhandler } from "../utils";
 
 export async function start({ interaction, args, locale }: { interaction: Message | ChatInputCommandInteraction; args?: string[]; locale: Locales }) {
   const options = Interactionhandler(interaction, args);
@@ -16,17 +16,22 @@ export async function start({ interaction, args, locale }: { interaction: Messag
     }
   }
 
-  commandName ? commandHelp(options, commandName, commands, locale) : helpMenu(options, commands, locale);
+  console.log();
+  commandName ? commandHelp(options, commandName, commands, locale) : helpMenu(options, commands, locale, interaction.client);
 }
 
-function helpMenu(options: any, commands: any, locale: Locales) {
+function helpMenu(options: any, commands: any, locale: Locales, client: Client) {
   const embed = new EmbedBuilder().setTitle(locale.embeds.help.title);
   const categories: any = {};
   Object.values(commands).forEach((array: any) => categories[array.category] = (categories[array.category] || []).concat(array));
+  Object.keys(categories).forEach(x => embed.addFields({ name: x, value: "```" + Object.values(categories[x]).map((element: any) => element.name).join(", ") + "```", inline: true }));
 
-  embed.addFields({ name: "general", value: "```" + Object.values(categories.general).map((element: any) => element.name).join(", ") + "```", inline: true });
-  embed.addFields({ name: "osu", value: "```" + Object.values(categories.osu).map((element: any) => element.name).join(", ") + "```", inline: true });
-  embed.addFields({ name: "owner", value: "```" + Object.values(categories.owner).map((element: any) => element.name).join(", ") + "```", inline: true });
+  const currUptimeMs = new Date(Bun.nanoseconds() / 1000000).getTime();
+  const currentUnixTime = Math.floor(Date.now() / 1000);
+  const uptime = Math.floor(currentUnixTime - (currUptimeMs / 1000));
+
+  embed.addFields({ name: locale.embeds.help.botInfo, value: `- ${locale.embeds.help.botServerCount(client.guilds.cache.size.toLocaleString())}.\n- ${locale.embeds.help.botUptime(`<t:${uptime}:R>`)}`, inline: false });
+  embed.addFields({ name: locale.embeds.help.commands, value: getWholeDb("commands").sort((a: any, b: any) => b.count - a.count).slice(0, 5).map((x: any) => `- ${x.name} :  ${x.count}`).join("\n") });
   options.reply({ embeds: [embed] });
 }
 
