3982529cbbce6108d766214171e5041c648800e7
YoruNoKen <m.f.fetvaci@hotmail.com>
2023-12-13 19:59:47 +0300

    testing

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ import type { ExtendedClient } from "./Structure/index";
 
 export const defaultPrefix = "!";
 export const serverJoinMessage = (client: ExtendedClient) => `Hello, I'm ${client.user?.username}! I'm a Discord bot developed by @yorunoken for osu!game. I can fetch recent plays and display top scores!\nHere are my commands:\n\`\`\`none\`\`\`\n If you come across any issues or bugs, contact my owner at @yorunoken on Discord or @ken_yoru on Twiter.`;
+ 
