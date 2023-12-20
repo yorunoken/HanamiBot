b376f03a0aef0460467adac4720c113ceab79c35
YoruNoKen <m.f.fetvaci@hotmail.com>
2023-12-20 03:54:10 +0300

    refactor link c omand

--- src/Helpers/link.ts
+++ src/Helpers/link.ts
@@ -1,39 +1,36 @@
-import { ChatInputCommandInteraction, EmbedBuilder, Message, User } from "discord.js";
+import { getUsernameFromArgs, insertData, interactionhandler } from "../utils";
+import { EmbedBuilder } from "discord.js";
 import { v2 } from "osu-api-extended";
-import { Locales } from "../Structure/index";
-import { getUser, getUsernameFromArgs, insertData, interactionhandler } from "../utils";
+import type { ChatInputCommandInteraction, Message, User } from "discord.js";
+import type { Locales } from "../Structure/index";
 
-export async function start(interaction: ChatInputCommandInteraction | Message, locale: Locales, args?: string[]) {
-  const options = interactionhandler(interaction, args);
-  const { reply } = options;
+export async function start(interaction: ChatInputCommandInteraction | Message, locale: Locales, args?: Array<string>): Promise<void> {
+    const { reply, userArgs: username, author } = interactionhandler(interaction, args);
 
-  const username = options.userArgs;
-  if (username.length === 0) {
-    return reply(locale.embeds.provideUsername);
-  }
+    if (username.length === 0) {
+        await reply(locale.embeds.provideUsername);
+        return;
+    }
 
-  const userOptions = getUsernameFromArgs({} as User, username);
-  if (userOptions?.user.status === false) {
-    return reply(locale.fails.linkFail);
-  }
+    const userOptions = getUsernameFromArgs({} as User, username);
+    if (!userOptions) return;
+    if (userOptions.user === undefined || typeof userOptions.user === "object") {
+        await reply(locale.fails.linkFail);
+        return;
+    }
 
-  const author = options.author;
-  const user = await v2.user.details(userOptions?.user, "osu");
-  if (!user.id) {
-    return options.reply(locale.fails.userDoesntExist(userOptions?.user));
-  }
+    const user = await v2.user.details(userOptions.user, "osu");
+    if (!user.id) {
+        await reply(locale.fails.userDoesntExist(userOptions.user));
+        return;
+    }
 
-  let currentDocs = await getUser(author.id);
-  if (!currentDocs) {
-    currentDocs = {};
-    currentDocs.data = JSON.stringify({ banchoId: user.id });
-    insertData({ table: "users", id: author.id, data: currentDocs.data });
-  }
-  let data = JSON.parse(currentDocs.data);
-  data.banchoId = user.id;
+    insertData({ table: "users", id: author.id, data: [ { name: "banchoId", value: user.id } ] });
 
-  insertData({ table: "users", id: author.id, data: JSON.stringify(data) });
-
-  const embed = new EmbedBuilder().setColor("Green").setTitle(locale.misc.success).setDescription(locale.embeds.link.success(author.id, user.username)).setThumbnail(user.avatar_url);
-  await reply({ embeds: [embed] });
+    const embed = new EmbedBuilder()
+        .setColor("Green")
+        .setTitle(locale.misc.success)
+        .setDescription(locale.embeds.link.success(author.id, user.username))
+        .setThumbnail(user.avatar_url);
+    await reply({ embeds: [embed] });
 }
