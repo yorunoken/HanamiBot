679db25510e013b0014adbab74b64ef91b5a7a9c
YoruNoKen <m.f.fetvaci@hotmail.com>
2023-12-20 03:54:20 +0300

    removed unneeded import from extended client

--- src/Structure/ExtendedClient.ts
+++ src/Structure/ExtendedClient.ts
@@ -1,6 +1,5 @@
 import { GuildCreateEvent, InteractionCreateEvent, MessageCreateEvent, ReadyEvent } from "../Events";
 import { getServer, getServersInBulk, insertData } from "../utils";
-import { LocalizationManager } from "../locales";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
 import { Client } from "discord.js";
