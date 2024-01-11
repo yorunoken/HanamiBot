81274b509111ac243559e12349a1d249515294ef
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-01-11 05:17:26 +0300

    updated profile commands

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -1,5 +1,32 @@
+import { getUser } from "../utils/database";
 import { ApplicationCommandOptionType } from "lilybird";
+import type { ApplicationCommandData, Interaction } from "lilybird";
 import type { SlashCommand } from "@lilybird/handlers";
+
+async function run(interaction: Interaction<ApplicationCommandData>): Promise<void> {
+    if (!interaction.inGuild()) return;
+    await interaction.deferReply();
+
+    const { data, member } = interaction;
+
+    const username = data.getString("username") ?? getUser(member.user.id);
+    const mode = data.getString("mode") ?? "osu";
+
+    const discordUserId = data.getUser("discord");
+    const discordUser = getUser(discordUserId ?? "");
+
+    console.log(discordUser, username);
+
+    if (typeof discordUser === "undefined" && typeof username !== "undefined") {
+        await interaction.editReply(`The user <@${discordUserId}> hasn't linked their account to the bot yet! mode: ${mode}`);
+        return;
+    }
+
+    if (typeof username === "undefined") {
+        await interaction.editReply(`Please link your account to the bot using /link! mode: ${mode}`);
+        return;
+    }
+}
 
 export default {
     post: "GLOBAL",
@@ -10,23 +37,20 @@ export default {
             {
                 type: ApplicationCommandOptionType.STRING,
                 name: "username",
-                description: "Specify an osu! username",
-                choices: [ { name: "osu", value: "osu" }, { name: "mania", value: "mania" }, { name: "taiko", value: "taiko" }, { name: "ctb", value: "fruits" } ]
+                description: "Specify an osu! username"
             },
             {
                 type: ApplicationCommandOptionType.STRING,
+                name: "mode",
+                description: "Specify an osu! mode",
+                choices: [ { name: "osu", value: "osu" }, { name: "mania", value: "mania" }, { name: "taiko", value: "taiko" }, { name: "ctb", value: "fruits" } ]
+            },
+            {
+                type: ApplicationCommandOptionType.USER,
                 name: "discord",
-                description: "Specify an osu! username"
+                description: "Specify a linked Discord user"
             }
         ]
     },
-    run: async (interaction) => {
-        await interaction.deferReply();
-
-        const { ws, rest } = await interaction.client.ping();
-
-        await interaction.editReply({
-            content: `🏓 WebSocket: \`${ws}ms\` | Rest: \`${rest}ms\``
-        });
-    }
+    run
 } satisfies SlashCommand;
