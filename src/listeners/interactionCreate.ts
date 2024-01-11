89be6c1da4191cd3cd551dddd8bd9af09d7f6308
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-01-11 05:15:47 +0300

    move interaction components inside of if statement
    this is so I can know what I'm doing when Lilybird is exporting the types that I need

--- src/listeners/interactionCreate.ts
+++ src/listeners/interactionCreate.ts
@@ -1,41 +1,41 @@
-import { insertData } from "../utils";
+import { insertData } from "../utils/database";
 import { v2 } from "osu-api-extended";
 import { EmbedType } from "lilybird";
 import type { Interaction } from "lilybird";
 import type { Event } from "@lilybird/handlers";
 
 async function run(interaction: Interaction): Promise<void> {
-    if (!interaction.isMessageComponentInteraction()) return;
+    if (interaction.isMessageComponentInteraction()) {
+        if (interaction.data.id === "verify") {
+            await interaction.deferReply(true);
 
-    if (interaction.data.id === "verify") {
-        await interaction.deferReply(true);
+            const { embeds, author } = interaction.message;
+            const messageEmbed = embeds?.[0];
+            if (!messageEmbed || author.id !== interaction.client.user.id) return;
 
-        const { embeds, author } = interaction.message;
-        const messageEmbed = embeds?.[0];
-        if (!messageEmbed || author.id !== interaction.client.user.id) return;
+            const { fields } = messageEmbed;
+            if (!fields) return;
 
-        const { fields } = messageEmbed;
-        if (!fields) return;
+            const discordId = fields[0].value;
+            const osuId = fields[1].value;
 
-        const discordId = fields[0].value;
-        const osuId = fields[1].value;
+            const user = await v2.user.details(osuId);
+            if (!user.id) {
+                await interaction.editReply("It seems like this a user with this osu! ID doesn't exist.. They might've gotten banned.");
+                return;
+            }
 
-        const user = await v2.user.details(osuId);
-        if (!user.id) {
-            await interaction.editReply("It seems like this a user with this osu! ID doesn't exist.. They might've gotten banned.");
-            return;
+            insertData({ table: "users", id: discordId, data: [ { name: "banchoId", value: osuId } ] });
+
+            const embed = {
+                type: EmbedType.Rich,
+                title: "Success!",
+                description: `Successfully linked <@${discordId}> with ${user.username}`,
+                children: { data: { url: user.avatar_url }, type: "thumbnail" }
+            };
+
+            await interaction.editReply({ embeds: [embed] });
         }
-
-        insertData({ table: "users", id: discordId, data: [ { name: "banchoId", value: osuId } ] });
-
-        const embed = {
-            type: EmbedType.Rich,
-            title: "Success!",
-            description: `Successfully linked <@${discordId}> with ${user.username}`,
-            children: { data: { url: user.avatar_url }, type: "thumbnail" }
-        };
-
-        await interaction.editReply({ embeds: [embed] });
     }
 }
 
