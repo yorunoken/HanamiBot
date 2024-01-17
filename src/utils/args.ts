b37951b8dbc5ec2c71ce30201b6bf269c9d3ae16
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-01-17 22:06:49 +0300

    new utils

--- src/utils/args.ts
+++ src/utils/args.ts
@@ -1,6 +1,6 @@
 import { getUser } from "./database";
 import { InteractionType } from "lilybird";
-import type { modes } from "../types/osu";
+import type { Modes } from "../types/osu";
 import type { CommandArgs, ParsedArgs, User } from "../types/commandArgs";
 import type { ApplicationCommandData, Interaction, Message } from "lilybird";
 
@@ -8,18 +8,23 @@ export function getCommandArgs(interaction: Interaction<ApplicationCommandData> 
     if (interaction.type === InteractionType.APPLICATION_COMMAND && interaction.inGuild()) {
         const { data, member } = interaction;
 
-        const username = data.getString("username") ?? getUser(member.user.id)?.banchoId;
+        const userArg = data.getString("username");
+        const userId = getUser(member.user.id)?.banchoId;
         const discordUserId = data.getUser("discord");
         const discordUser = getUser(discordUserId ?? "")?.banchoId;
 
         // why cpol
-        const mode: modes = data.getString("mode") as modes | undefined ?? "osu";
+        const mode: Modes = data.getString("mode") as Modes | undefined ?? "osu";
 
-        const user: User = discordUser
-            ? { type: "success", banchoId: discordUser, mode }
-            : username
-                ? { type: "success", banchoId: username, mode }
-                : { type: "fail", failMessage: discordUserId ? `The user <@${discordUserId}> hasn't linked their account to the bot yet!` : "Please link your account to the bot using /link!" };
+        const user: User = discordUserId
+            ? discordUser
+                ? { type: "success", banchoId: discordUser, mode }
+                : { type: "fail", failMessage: discordUserId ? `The user <@${discordUserId}> hasn't linked their account to the bot yet!` : "Please link your account to the bot using /link!" }
+            : userArg
+                ? { type: "success", banchoId: userArg, mode }
+                : userId
+                    ? { type: "success", banchoId: userId, mode }
+                    : { type: "fail", failMessage: "Please link your account to the bot using /link!" };
 
         return { user };
     }
