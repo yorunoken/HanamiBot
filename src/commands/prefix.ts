6fb8c9be329ae2475abedbc56ed8bc2c4a09e382
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-01-27 18:39:42 +0300

    fixed subcommands it literally was just missing `deferReply()` LMAO

--- src/commands/prefix.ts
+++ src/commands/prefix.ts
@@ -1,6 +1,7 @@
 import { getServer, insertData } from "../utils/database";
-import { updatePrefixCache } from "../cache";
-import { ApplicationCommandOptionType } from "lilybird";
+import { DEFAULT_PREFIX, MAX_AMOUNT_OF_PREFIXES } from "../utils/constants";
+import { updatePrefixCache } from "../listeners/guildCreate";
+import { ApplicationCommandOptionType, EmbedType } from "lilybird";
 import type { ApplicationCommandData, GuildInteraction, Interaction } from "lilybird";
 import type { SlashCommand } from "@lilybird/handlers";
 
@@ -11,12 +12,8 @@ const commands: Record<string, ({ prefix, interaction, guildId }: { prefix?: str
 };
 
 async function run(interaction: Interaction<ApplicationCommandData>): Promise<void> {
-    console.log("la");
-    await interaction.editReply("test 1");
-
+    await interaction.deferReply();
     if (!interaction.inGuild()) return;
-    console.log("ww");
-    await interaction.editReply("test 2");
 
     const subcommand = interaction.data.subCommand ?? "list";
 
@@ -27,14 +24,19 @@ async function run(interaction: Interaction<ApplicationCommandData>): Promise<vo
 
 async function add({ prefix, interaction, guildId }: { prefix?: string, interaction: GuildInteraction<ApplicationCommandData>, guildId: string }): Promise<void> {
     const server = getServer(guildId);
-    if (!server || !prefix) return;
+    if (!prefix || !server) return;
     let { prefixes } = server;
 
-    if (prefixes !== null)
-        prefixes = JSON.parse(prefixes.toString()) as Array<string>;
+    if (prefixes !== null && !Array.isArray(prefixes))
+        prefixes = JSON.parse(prefixes) as Array<string>;
 
-    if (prefixes && prefixes.some((pref) => pref === prefix)) {
-        await interaction.editReply(`The prefix, ${prefix}, is already in the prefixes list.`);
+    if (prefixes && prefixes.length > MAX_AMOUNT_OF_PREFIXES) {
+        await interaction.editReply(`**The maximum amount of prefixes allowed is \`${MAX_AMOUNT_OF_PREFIXES}\`. You can remove a prefix using \`/prefix remove\`**`);
+        return;
+    }
+
+    if (prefixes?.some((pref) => pref === prefix)) {
+        await interaction.editReply(`**The prefix \`${prefix}\` is already in the prefixes list. You can look at current prefixes by using \`/prefix list\`**`);
         return;
     }
 
@@ -43,19 +45,53 @@ async function add({ prefix, interaction, guildId }: { prefix?: string, interact
     insertData({ table: "servers", id: guildId, data: [ { name: "prefixes", value: JSON.stringify(newPrefixes) } ] });
     updatePrefixCache(guildId, newPrefixes);
 
-    await interaction.editReply(`The prefix, ${prefix}, has been added to the list.`);
+    await interaction.editReply(`**The prefix \`${prefix}\` has been added to the list.**`);
     return;
 }
 
 async function remove({ prefix, interaction, guildId }: { prefix?: string, interaction: GuildInteraction<ApplicationCommandData>, guildId: string }): Promise<void> {
-    // implement function
+    const server = getServer(guildId);
+    if (!prefix || !server) return;
+    let { prefixes } = server;
+
+    if (prefixes === null) {
+        await interaction.editReply("**There aren't any prefixes on this server. You can add a new prefixes by using `/prefix add`**");
+        return;
+    }
+    prefixes = JSON.parse(prefixes.toString()) as Array<string>;
+
+    if (!prefixes.some((pref) => pref === prefix)) {
+        await interaction.editReply(`**The prefix \`${prefix}\` is not in the prefixes list. You can look at current prefixes by using \`/prefix list\`**`);
+        return;
+    }
+
+    const newPrefixes = prefixes.filter((item) => item !== prefix);
+    insertData({ table: "servers", id: guildId, data: [ { name: "prefixes", value: JSON.stringify(newPrefixes) } ] });
+    updatePrefixCache(guildId, newPrefixes);
+
+    let message = `**The prefix \`${prefix}\` has been removed from the list.**`;
+    if (newPrefixes.length === 0)
+        message = `**__Warning__:\nThere are no more custom prefixes on the server left. The default prefix is \`${DEFAULT_PREFIX.join("")}\`**`;
+
+    await interaction.editReply(message);
+    return;
 }
 
 async function list({ interaction, guildId }: { interaction: GuildInteraction<ApplicationCommandData>, guildId: string }): Promise<void> {
-    // implement function
+    const server = getServer(guildId);
+    if (!server) return;
+
+    let { prefixes } = server;
+
+    if (prefixes === null) {
+        await interaction.editReply(`**There aren't any custom prefixes on this server. The default is \`${DEFAULT_PREFIX.join("")}\`**`);
+        return;
+    }
+    prefixes = JSON.parse(prefixes.toString()) as Array<string>;
+
+    await interaction.editReply({ embeds: [ { type: EmbedType.Rich, title: "Success!", description: `Below are the current prefixes of this server. \n- \`${prefixes.join("`\n- `")}\`` } ] });
 }
 
-// This is RIP until Didas fixes subcommands
 export default {
     post: "GLOBAL",
     data: {
