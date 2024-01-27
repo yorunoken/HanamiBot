2dbc6e3576caebc1038686faf8a69dcd826fe369
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-01-27 18:40:39 +0300

    added new constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,6 @@
 export const linkSlash = "</link:1193307769839829062>";
 export const unlinkSlash = "</unlink:1197252800451715116>";
+
+export const DEFAULT_PREFIX = [";"];
+export const MAX_AMOUNT_OF_PREFIXES = 10;
+
