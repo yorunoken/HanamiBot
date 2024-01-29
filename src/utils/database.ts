83c35496ce079acee191d3247a656d500c3609db
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-01-29 13:09:05 +0300

    added getMap function

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,5 +1,5 @@
 import db from "../data.db" with { type: "sqlite" };
-import type { DbServer, DbUser } from "../types/database";
+import type { DbMap, DbServer, DbUser } from "../types/database";
 
 export function getUser(id: string | number): DbUser | undefined {
     return db.prepare("SELECT * FROM users WHERE id = ?").get(id) as DbUser;
@@ -11,6 +11,10 @@ export function removeUser(id: string | number): void {
 
 export function getServer(id: string | number): DbServer | undefined {
     return db.prepare("SELECT * FROM servers WHERE id = ?").get(id) as DbServer;
+}
+
+export function getMap(id: string | number): DbMap | undefined {
+    return db.prepare("SELECT * FROM maps WHERE id = ?").get(id) as DbMap;
 }
 
 export function insertData({ table, id, data }: { table: string, id: string | number, data: Array<{ name: string, value: string | number | null }> }): void {
