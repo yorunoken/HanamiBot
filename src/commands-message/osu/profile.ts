b9ab1e707b0a2a1ab153ae8532141b86fde13375
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-02-03 20:50:23 +0300

    ff

---
+++ src/commands-message/osu/profile.ts
@@ -0,0 +1,33 @@
+import { parseOsuArguments } from "../../utils/args";
+import { profileBuilder } from "../../embed-builders/profile";
+import { client } from "../../utils/initalize";
+import type { MessageCommand } from "../../types/commands";
+import type { Modes } from "../../types/osu";
+import type { Message } from "lilybird";
+
+async function run({ message, args, commandName }: { message: Message, args: Array<string>, commandName: string }): Promise<void> {
+    const channel = await message.fetchChannel();
+
+    const { user } = parseOsuArguments(message, args, commandName as Modes);
+    if (user.type === "fail") {
+        await channel.send(user.failMessage);
+        return;
+    }
+
+    const osuUser = await client.users.getUser(user.banchoId, { urlParams: { mode: user.mode } });
+    if (!osuUser.id) {
+        await channel.send("This user does not exist.");
+        return;
+    }
+
+    const embed = profileBuilder(osuUser, user.mode);
+    await channel.send({ embeds: [embed] });
+}
+
+export default {
+    name: "profile",
+    aliases: ["osu", "mania", "taiko", "fruits"],
+    description: "Display statistics of a user.",
+    cooldown: 1000,
+    run
+} satisfies MessageCommand;
