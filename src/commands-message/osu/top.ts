109acea5c6d1a2eb0a576635f31fd1cf0acffebe
YoruNoKen <m.f.fetvaci@hotmail.com>
2024-02-05 02:35:44 +0300

    adding `top` command

---
+++ src/commands-message/osu/top.ts
@@ -0,0 +1,50 @@
+import { parseOsuArguments } from "../../utils/args";
+import { client } from "../../utils/initalize";
+import { playBuilder } from "../../embed-builders/plays";
+import type { MessageCommand } from "../../types/commands";
+import type { Modes } from "../../types/osu";
+import type { Message } from "lilybird";
+
+const modeAliases: Record<string, { mode: Modes }> = {
+    top: { mode: "osu" },
+    toposu: { mode: "osu" },
+    toptaiko: { mode: "taiko" },
+    topmania: { mode: "mania" },
+    topcatch: { mode: "fruits" },
+    topctb: { mode: "fruits" },
+
+    t: { mode: "osu" },
+    to: { mode: "osu" },
+    tt: { mode: "taiko" },
+    tm: { mode: "mania" },
+    tc: { mode: "fruits" },
+    tctb: { mode: "fruits" }
+};
+
+async function run({ message, args, commandName, index = 0 }: { message: Message, args: Array<string>, commandName: string, index: number | undefined }): Promise<void> {
+    const channel = await message.fetchChannel();
+
+    const { mode } = modeAliases[commandName];
+    const { user, mods } = parseOsuArguments(message, args, mode);
+    if (user.type === "fail") {
+        await channel.send(user.failMessage);
+        return;
+    }
+
+    const osuUser = await client.users.getUser(user.banchoId, { urlParams: { mode: user.mode } });
+    if (!osuUser.id) {
+        await channel.send("This user does not exist.");
+        return;
+    }
+
+    const embeds = await playBuilder({ user: osuUser, mode: user.mode, type: "best", index, mods });
+    await channel.send({ embeds });
+}
+
+export default {
+    name: "top",
+    aliases: Object.keys(modeAliases),
+    description: "Display top play(s) of a user.",
+    cooldown: 1000,
+    run
+} satisfies MessageCommand;
