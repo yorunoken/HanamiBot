f2a859b8058a8f2e55bf6742691500c5f8707795
Yoru <m.f.fetvaci@hotmail.com>
2024-02-08 21:47:18 +0300

    new ENV parameter

--- globals.d.ts
+++ globals.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
     interface ProcessEnv {
     DISCORD_BOT_TOKEN: string;
+    ACCESS_TOKEN: string;
     CLIENT_SECRET: string;
     CLIENT_ID: string;
     CALLBACK_URL: string;
