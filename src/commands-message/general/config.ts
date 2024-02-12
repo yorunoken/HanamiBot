11fec3bb73fc22435e24eddeea9df072c89aec4e
Yoru <m.f.fetvaci@hotmail.com>
2024-02-12 11:41:36 +0300

    forgot to import `slashCommandsIds` on some cmds

--- src/commands-message/general/config.ts
+++ src/commands-message/general/config.ts
@@ -1,11 +1,12 @@
+import { slashCommandsIds } from "../../utils/ cache";
 import type { MessageCommand } from "../../types/commands";
 import type { Message } from "lilybird";
 
 export default {
-    name: "ping",
+    name: "config",
     description: "Changes the config files of the user",
     cooldown: 1000,
     run: async ({ message }: { message: Message }) => {
-        await message.reply("This command has been deprecated. Use /config instead.");
+        await message.reply(`This command has been deprecated. Use ${slashCommandsIds.get("config")}instead.`);
     }
 } satisfies MessageCommand;
