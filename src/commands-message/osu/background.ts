4ce2f7f1d554ff829d3eeac19ac14e8efc6ef85e
Yoru <yorunoken@skiff.com>
2024-02-20 00:11:45 +0300

    add background command (#110)

---
+++ src/commands-message/osu/background.ts
@@ -0,0 +1,57 @@
+import { parseOsuArguments } from "../../utils/args";
+import { getBeatmapIdFromContext } from "../../utils/osu";
+import { Mode } from "../../types/osu";
+import { EmbedBuilderType } from "../../types/embedBuilders";
+import { client } from "../../utils/initalize";
+import { backgroundBuilder } from "../../embed-builders/backgroundBuilder";
+import { EmbedType } from "lilybird";
+import type { Message } from "lilybird";
+import type { MessageCommand } from "../../types/commands";
+
+export default {
+    name: "background",
+    aliases: ["bg", "background"],
+    description: "Display background of a beatmap.",
+    cooldown: 1000,
+    run
+} satisfies MessageCommand;
+
+async function run({ message, args }: { message: Message, args: Array<string> }): Promise<void> {
+    const channel = await message.fetchChannel();
+
+    const { user } = parseOsuArguments(message, args, Mode.OSU);
+
+    const beatmapId = user.beatmapId ?? await getBeatmapIdFromContext({ message, client: message.client });
+    if (typeof beatmapId === "undefined" || beatmapId === null) {
+        await channel.send({
+            embeds: [
+                {
+                    type: EmbedType.Rich,
+                    title: "Uh oh! :x:",
+                    description: "It seems like the beatmap ID couldn't be found :(\n"
+                }
+            ]
+        });
+        return;
+    }
+
+    const beatmap = await client.beatmaps.getBeatmap(Number(beatmapId));
+    if (!beatmap.id) {
+        await channel.send({
+            embeds: [
+                {
+                    type: EmbedType.Rich,
+                    title: "Uh oh! :x:",
+                    description: "It seems like this beatmap doesn't exist! :("
+                }
+            ]
+        });
+        return;
+    }
+
+    const embeds = backgroundBuilder({
+        type: EmbedBuilderType.MAP,
+        beatmap
+    });
+    await channel.send({ embeds });
+}
---
+++ src/commands/osu/background.ts
@@ -0,0 +1,66 @@
+import { getCommandArgs } from "../../utils/args";
+import { getBeatmapIdFromContext } from "../../utils/osu";
+import { EmbedBuilderType } from "../../types/embedBuilders";
+import { backgroundBuilder } from "../../embed-builders/backgroundBuilder";
+import { client } from "../../utils/initalize";
+import { ApplicationCommandOptionType, EmbedType } from "lilybird";
+import type { ApplicationCommandData, Interaction } from "lilybird";
+import type { SlashCommand } from "@lilybird/handlers";
+
+export default {
+    post: "GLOBAL",
+    data: {
+        name: "background",
+        description: "Display background of a beatmap.",
+        options: [
+            {
+                type: ApplicationCommandOptionType.STRING,
+                name: "map",
+                description: "Specify a beatmap link (eg: https://osu.ppy.sh/b/72727)"
+            }
+        ]
+    },
+    run
+} satisfies SlashCommand;
+
+async function run(interaction: Interaction<ApplicationCommandData>): Promise<void> {
+    if (!interaction.inGuild()) return;
+    await interaction.deferReply();
+
+    const args = getCommandArgs(interaction);
+
+    if (typeof args === "undefined") return;
+    const { user } = args;
+
+    const beatmapId = user.beatmapId ?? await getBeatmapIdFromContext({ channelId: interaction.channelId, client: interaction.client });
+    if (typeof beatmapId === "undefined" || beatmapId === null) {
+        await interaction.editReply({
+            embeds: [
+                {
+                    type: EmbedType.Rich,
+                    title: "Uh oh! :x:",
+                    description: "It seems like the beatmap ID couldn't be found :(\n"
+                }
+            ]
+        });
+        return;
+    }
+
+    const beatmap = await client.beatmaps.getBeatmap(Number(beatmapId));
+    if (!beatmap.id) {
+        await interaction.editReply({
+            embeds: [
+                {
+                    type: EmbedType.Rich,
+                    title: "Uh oh! :x:",
+                    description: "It seems like this beatmap doesn't exist! :("
+                }
+            ]
+        });
+        return;
+    }
+
+    const embeds = backgroundBuilder({ type: EmbedBuilderType.MAP, beatmap });
+    await interaction.editReply({ embeds });
+}
+
