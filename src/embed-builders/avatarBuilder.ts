cbaa1a2086622c69c9dad52161e71ddc5e604b99
Yoru <m.f.fetvaci@hotmail.com>
2024-02-20 13:14:22 +0300

    add url links to avatar and background builder

--- src/embed-builders/avatarBuilder.ts
+++ src/embed-builders/avatarBuilder.ts
@@ -6,5 +6,5 @@ import type { AvatarBuilderOptions } from "../types/embedBuilders";
 export function avatarBuilder({ user }: AvatarBuilderOptions): Array<EmbedStructure> {
     const profile = getProfile(user, Mode.OSU);
 
-    return [ { author: { name: `Profile avatar of ${profile.username}`, icon_url: profile.flagUrl }, image: { url: profile.avatarUrl } } ];
+    return [ { author: { name: `Profile avatar of ${profile.username}`, icon_url: profile.flagUrl, url: profile.userUrl }, image: { url: profile.avatarUrl } } ];
 }
