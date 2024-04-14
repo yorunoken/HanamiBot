aa2e78de78057763ee8ba2a6cacd5387759b5942
YoruNoken <yorunoken@skiff.com>
2024-04-14 21:20:18 +0300

    add new guild entries in database

--- src/listeners/guildCreate.ts
+++ src/listeners/guildCreate.ts
@@ -1,13 +1,41 @@
 import { getServer, insertData } from "@utils/database";
+import type { NewGuild } from "@lilybird/transformers";
+import type { UnavailableGuildStructure } from "lilybird";
 import type { Event } from "@lilybird/handlers";
 
 export const prefixesCache = new Map<string, Array<string>>();
 
 export default {
     event: "guildCreate",
-    run: (guild) => {
+    run: (guild: UnavailableGuildStructure | NewGuild) => {
         const document = getServer(guild.id);
-        if (document === null) insertData({ table: "servers", id: guild.id, data: [ { name: "prefixes", value: null } ] });
-        else if (document.prefixes !== null) prefixesCache.set(guild.id, document.prefixes);
+
+        if (document === null) {
+            const basicData: Array<{ name: string, value: string | number | null }> = [ { name: "prefixes", value: null } ];
+
+            if ("name" in guild) {
+                basicData.push(
+                    { name: "name", value: guild.name },
+                    { name: "owner_id", value: guild.ownerId },
+                    { name: "joined_at", value: guild.joinedAt }
+                );
+            }
+
+            insertData({ table: "servers", id: guild.id, data: basicData });
+        } else if ("name" in guild) {
+            insertData({
+                table: "servers",
+                id: guild.id,
+                data: [
+                    { name: "name", value: guild.name },
+                    { name: "owner_id", value: guild.ownerId },
+                    { name: "joined_at", value: guild.joinedAt }
+                ]
+            });
+        }
+
+        // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+        if (document !== null && document.prefixes !== null)
+            prefixesCache.set(guild.id, document.prefixes);
     }
 } satisfies Event<"guildCreate">;
