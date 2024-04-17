00835d2c2ba30ad9a2b4647172019cd67fb90533
Yoru <m.f.fetvaci@hotmail.com>
2024-04-17 22:33:21 +0300

    add new table to database

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,13 +1,13 @@
 import db from "../data.db" with { type: "sqlite" };
-import type { DatabaseMap, DatabaseGuild, DatabaseUser, DatabaseCommands } from "@type/database";
+import type { DatabaseMap, DatabaseGuild, DatabaseUser, DatabaseCommands, DatabaseScores, DatabaseScoresPp } from "@type/database";
 
 export function query(str: string): unknown {
     return db.prepare(str).all();
 }
 
-export function getUser(id: string | number): DatabaseUser | null {
-    const data: DatabaseUser | null = db.prepare("SELECT * FROM users WHERE id = ?").get(id) as DatabaseUser | null;
-    if (typeof data?.score_embeds === "string") data.score_embeds = Number(data.score_embeds);
+export function getUser(id: string | number): DatabaseUser {
+    const data: DatabaseUser = db.prepare("SELECT * FROM users WHERE id = ?").get(id) as DatabaseUser;
+    if (typeof data.score_embeds === "string") data.score_embeds = Number(data.score_embeds);
 
     return data;
 }
@@ -16,9 +16,9 @@ export function removeUser(id: string | number): void {
     db.prepare("DELETE FROM users WHERE id = ?").run(id);
 }
 
-export function getServer(id: string | number): DatabaseGuild | null {
-    const data: DatabaseGuild | null = db.prepare("SELECT * FROM servers WHERE id = ?").get(id) as DatabaseGuild | null;
-    if (typeof data?.prefixes === "string") data.prefixes = JSON.parse(data.prefixes) as Array<string>;
+export function getServer(id: string | number): DatabaseGuild {
+    const data: DatabaseGuild = db.prepare("SELECT * FROM servers WHERE id = ?").get(id) as DatabaseGuild;
+    if (typeof data.prefixes === "string") data.prefixes = JSON.parse(data.prefixes) as Array<string>;
 
     return data;
 }
@@ -32,24 +32,32 @@ export function getRowCount(table: string): number {
     return count["COUNT(*)"];
 }
 
-export function getMap(id: string | number): DatabaseMap | null {
+export function getMap(id: string | number): DatabaseMap {
     return db.prepare("SELECT * FROM maps WHERE id = ?").get(id) as DatabaseMap;
 }
 
-export function getCommand(id: string | number): DatabaseCommands | null {
+export function getCommand(id: string | number): DatabaseCommands {
     return db.prepare("SELECT * FROM commands WHERE id = ?").get(id) as DatabaseCommands;
 }
 
-export function insertData({ table, id, data }: { table: string, id: string | number, data: Array<{ name: string, value: string | number | null }> }): void {
+export function getScores(id: string | number): DatabaseScores {
+    return db.prepare("SELECT * FROM osu_scores WHERE id = ?").get(id) as DatabaseScores;
+}
+
+export function getScoresPp(id: string | number): DatabaseScoresPp {
+    return db.prepare("SELECT * FROM osu_scores_pp WHERE id = ?").get(id) as DatabaseScoresPp;
+}
+
+export function insertData({ table, id, data }: { table: string, id: string | number, data: Array<{ name: string, value: string | number | boolean | null }> }, ignore?: boolean): void {
     const setClause = data.map((item) => `${item.name} = ?`).join(", ");
-    const values: Array<string | number | null> = data.map((item) => item.value);
+    const values: Array<string | number | boolean | null> = data.map((item) => item.value);
 
     const existingRow = db.prepare(`SELECT * FROM ${table} WHERE id = ?`).get(id);
     if (!existingRow) {
         const fields: Array<string> = data.map((item) => item.name);
         const placeholders = fields.map(() => "?").join(", ");
 
-        db.prepare(`INSERT OR REPLACE INTO ${table} (id, ${fields.join(", ")}) values (?, ${placeholders});`)
+        db.prepare(`INSERT OR ${ignore ? "IGNORE" : "REPLACE"} INTO ${table} (id, ${fields.join(", ")}) values (?, ${placeholders});`)
             .run(id, ...values);
     }
 
