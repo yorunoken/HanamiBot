74cc98d8c569855a1756b2432eac65cb9c41726e
Yoru <m.f.fetvaci@hotmail.com>
2024-05-03 22:22:56 +0300

    remove debug listener

--- src/index.ts
+++ src/index.ts
@@ -60,8 +60,6 @@ const listeners = await createHandler({
 });
 
 await createClient({
-    attachDebugListener: true,
-    useDebugRest: true,
     token: process.env.DISCORD_BOT_TOKEN,
     caching: {
         transformerTypes: { channel: Channel, guild: Guild, voiceState: GuildVoiceChannel },
