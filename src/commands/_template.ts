3b97d692b3926cd37bcb8e7acab30b642a0942fb
Yoru <me@yorunoken.com>
2025-09-02 20:54:29 +0300

    update template command desc

--- src/commands/_template.ts
+++ src/commands/_template.ts
@@ -1,3 +1,5 @@
+// Put this file into a folder inside `commands`
+
 import { CommandData, MessageCommand, ApplicationCommand } from "@type/commands";
 
 export async function runMessage({ message }: MessageCommand) {
@@ -13,5 +15,5 @@ export async function runApplication({ interaction }: ApplicationCommand) {
 export const data = {
     name: "example",
     description: "An example command.",
-    hasPrefixVariant: true, // set to false if there's no prefix command for this.
+    hasPrefixVariant: true, // set to false if there's no prefix version of the command.
 } satisfies CommandData;
